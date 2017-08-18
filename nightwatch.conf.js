4a8d6815ea1ef94e6fcd3c0f356f9f69700cfd13
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-08-18 16:59:35 -0500

    Missed file name changes.

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,6 +1,6 @@
 const nightwatchConfig = require('./lib/nightwatch/nightwatch.config.js').default;
 const webpackConfig = require('./tests/test.config.js');
-const getTestDirectories = require('./src/nightwatch/monorepo-helpers.js').getTestDirectories;
+const getTestDirectories = require('./lib/nightwatch/setup-helper.js').getTestDirectories;
 
 const srcFolders = getTestDirectories(process.cwd());
 const config = nightwatchConfig(webpackConfig, srcFolders);
