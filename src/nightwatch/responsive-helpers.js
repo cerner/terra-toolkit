d33374ce75e063e8a5f0d97fa8f2d9f51f838a31
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-08-22 16:39:23 -0500

    Speed up tests.

--- src/nightwatch/responsive-helpers.js
+++ src/nightwatch/responsive-helpers.js
@@ -3,7 +3,7 @@ const screenWidth = (browser) => {
   return browser.globals.breakpoints[breakpoint][0];
 };
 
-const resize = (breakpoint, test) => (browser, done) => {
+const resize = breakpoint => (browser, done) => {
   const screenSize = browser.globals.breakpoints[breakpoint];
 
   if (!screenSize) {
@@ -12,28 +12,31 @@ const resize = (breakpoint, test) => (browser, done) => {
   }
 
   browser.url(browser.launchUrl);
-  browser.resizeWindow(screenSize[0], screenSize[1], () => {
-    test.apply(browser, [browser, done]);
-  });
+  browser.resizeWindow(screenSize[0], screenSize[1], done);
 };
 
-const resizeTo = (breakpoints, suite) =>
-  Object.keys(suite).reduce((tests, key) => {
-    const resizedTests = {};
-    breakpoints.forEach((breakpoint) => {
+const resizeTo = (breakpoints, suite) => {
+  const newTests = {};
+  breakpoints.forEach((breakpoint) => {
+    newTests[`Adjust Broswer to [@${breakpoint}]`] = () => resize(breakpoint);
+    Object.keys(suite).reduce((tests, key) => {
+      const resizedTests = {};
       const value = suite[key];
       if (typeof value === 'function') {
-        resizedTests[`${key} [@${breakpoint}]`] = resize(breakpoint, value);
+        resizedTests[`${key} [@${breakpoint}]`] = value;
       } else {
         // Maintains Nightwatch Test Tags
         resizedTests[key] = value;
       }
-    });
-    resizedTests.after = (browser, done) => {
-      browser.end(done);
-    };
-    return Object.assign(tests, resizedTests);
-  }, {});
+
+      resizedTests.after = (browser, done) => {
+        browser.end(done);
+      };
+      return Object.assign(tests, resizedTests);
+    }, newTests);
+  });
+  return newTests;
+};
 
 module.exports.resizeTo = resizeTo;
 module.exports.screenWidth = screenWidth;
