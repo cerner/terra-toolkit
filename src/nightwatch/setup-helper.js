bcb835c66d6ead07e3ca4073b0c78fecf09c2f2f
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2017-11-02 08:30:08 -0500

    Windows Dev Compatibility (#17)

--- src/nightwatch/setup-helper.js
+++ src/nightwatch/setup-helper.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const loadJsonFile = require('load-json-file');
 const globSync = require('glob').sync;
 
@@ -6,7 +7,7 @@ const getPackageTestDirectories = lernaJSON =>
   loadJsonFile.sync(lernaJSON).packages
     .map(globPath =>
       globSync(globPath)
-        .map(packagePath => `${packagePath}/tests/nightwatch/`)
+        .map(packagePath => path.join(packagePath, 'tests', 'nightwatch'))
         .filter(fs.existsSync))
     .reduce((a, b) => a.concat(b), []);
 
