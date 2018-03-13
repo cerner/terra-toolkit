9106bd83c1ce7d7706cad8f51db26594295a4e28
Matt Henkes <mjhenkes@gmail.com>
2018-03-13 09:15:12 -0500

    update warning to warn

--- src/wdio/services/WebpackDevServerService.js
+++ src/wdio/services/WebpackDevServerService.js
@@ -26,7 +26,7 @@ export default class WebDevServerService {
   startWebpackDevServer() {
     return new Promise((resolve, reject) => {
       // eslint-disable-next-line no-console
-      console.error('[WebDevService] WARNING: ExpressDevServer service should be used instead of the Terra provided WebpackDevServerService. See https://github.com/cerner/terra-toolkit/blob/master/docs/ExpressDevServerService.md');
+      console.warn('[WebDevService] WARNING: ExpressDevServer service should be used instead of the Terra provided WebpackDevServerService. See https://github.com/cerner/terra-toolkit/blob/master/docs/ExpressDevServerService.md');
       // eslint-disable-next-line no-console
       console.log('[WebDevService] Starting WebpackDevServer');
 
