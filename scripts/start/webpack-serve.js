dc9c89a36a1b7230439c643ce4d193d6bf35c71e
Matt Henkes <mjhenkes@gmail.com>
2018-04-11 11:31:51 -0500

    fix typo.

--- scripts/start/webpack-serve.js
+++ scripts/start/webpack-serve.js
@@ -1,7 +1,7 @@
 const serve = require('webpack-serve');
 
 const webpackServe = (options) => {
-  serve({ config: options.confg });
+  serve({ config: options.config });
 };
 
 module.exports = webpackServe;
