914086787713933b9b04cae146c57b52563fd4ea
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2018-06-04 08:37:36 -0500

    Update eslint.config.js

--- config/eslint.config.js
+++ config/eslint.config.js
@@ -3,15 +3,13 @@ module.exports = {
   "plugins": [
     "react",
     "jsx-a11y",
-    "import"
+    "import",
+    "compat"
   ],
   "env": {
     "browser": true,
     "jest": true
   },
-  "plugins": [
-    "compat"
-  ],
   "rules": {
     "max-len": "off", // Remove warnings on max line length exceeding 100 characters
     "react/require-default-props": "off", // Disabled the requirement to default all non-required props
