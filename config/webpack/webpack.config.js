19837472aaea6d73f979d3a28d6bb7451cae9d07
Tyler Biethman <tbiethman@users.noreply.github.com>
2018-06-12 08:46:15 -0500

    Updating webpack.config with PostCSS/UglifyJS corrections (#116)

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -51,6 +51,7 @@ const defaultWebpackConfig = (env = {}, argv = {}) => {
               ident: 'postcss',
               plugins() {
                 return [
+                  rtl(),
                   Autoprefixer({
                     browsers: [
                       'ie >= 10',
@@ -60,7 +61,6 @@ const defaultWebpackConfig = (env = {}, argv = {}) => {
                       'iOS >= 10',
                     ],
                   }),
-                  rtl(),
                 ];
               },
             },
@@ -129,6 +129,11 @@ const defaultWebpackConfig = (env = {}, argv = {}) => {
           cache: true,
           parallel: true,
           sourceMap: true,
+          uglifyOptions: {
+            compress: {
+              typeofs: false,
+            },
+          },
         }),
       ],
     },
