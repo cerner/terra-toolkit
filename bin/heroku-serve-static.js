bdf188684c08063bb42e85e43c049bd4ed108e3f
Manuel,Ryan <rfmanuel@gmail.com>
2018-07-20 11:20:26 -0500

    Fix heroku builds by creating a script with heroku defaults

--- bin/heroku-serve-static.js
+++ bin/heroku-serve-static.js
@@ -6,6 +6,7 @@ const loadWebpackConfig = require('../scripts/serve/loadWebpackConfig');
 serve({
   config: loadWebpackConfig(),
   host: '0.0.0.0',
+  // This environment variable is necessary for heroku deployments
   port: process.env.PORT,
   production: true,
   site: 'build',
