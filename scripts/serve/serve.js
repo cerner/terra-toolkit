b0043604eb81ba9d7b29f927d8d39616bb9d2cbc
Matt Henkes <mjhenkes@gmail.com>
2018-10-09 11:50:05 -0500

    Try out webpack-dev-server

--- scripts/serve/serve.js
+++ scripts/serve/serve.js
@@ -1,25 +1,42 @@
-const serve = require('webpack-serve');
+// const serve = require('webpack-serve');
+const WebpackDevServer = require('webpack-dev-server');
+const Webpack = require('webpack');
 
-const webpackServe = (options) => {
+const server = (options) => {
   const { port, host } = options;
   let config = options.config;
   if (typeof config === 'function') {
     config = config(undefined, { p: options.production });
   }
 
-  const serveConfig = {
-    config,
-    ...(port) && { port },
-    ...(host) && { host },
-    dev: {
-      stats: {
-        colors: true,
-        children: false,
-      },
+  const devServerOptions = Object.assign({}, config.devServer, {
+    stats: {
+      colors: true,
+      children: false,
     },
-  };
+  });
 
-  serve(serveConfig);
+  // const serveConfig = {
+  //   config,
+  //   ...(port) && { port },
+  //   ...(host) && { host },
+  //   dev: {
+  //     stats: {
+  //       colors: true,
+  //       children: false,
+  //     },
+  //   },
+  // };
+
+  // serve(serveConfig);
+
+  const compiler = Webpack(config);
+
+  const devServer = new WebpackDevServer(compiler, devServerOptions);
+
+  devServer.listen(port, host, () => {
+    console.log(`Starting server on http://${host}:${port}`);
+  });
 };
 
-module.exports = webpackServe;
+module.exports = server;
