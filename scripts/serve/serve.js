f660ada58be07ea0808c597492507b49a1aac0ad
Matt Henkes <mjhenkes@gmail.com>
2018-10-09 13:52:45 -0500

    Removing webpack-serve dependency

--- scripts/serve/serve.js
+++ scripts/serve/serve.js
@@ -1,40 +1,41 @@
-// const serve = require('webpack-serve');
+/* eslint-disable no-console */
 const WebpackDevServer = require('webpack-dev-server');
 const Webpack = require('webpack');
 
+// Create a webpack dev server instance.
 const server = (options) => {
   const { port, host } = options;
   let config = options.config;
+  // if config is a function, execute it with prod mode if applicable.
   if (typeof config === 'function') {
     config = config(undefined, { p: options.production });
   }
 
+  // pull the dev server options out of the webpack config. override host, port, and stats. SRY.
   const devServerOptions = Object.assign({}, config.devServer, {
+    host,
+    port,
     stats: {
       colors: true,
       children: false,
     },
   });
 
-  // const serveConfig = {
-  //   config,
-  //   ...(port) && { port },
-  //   ...(host) && { host },
-  //   dev: {
-  //     stats: {
-  //       colors: true,
-  //       children: false,
-  //     },
-  //   },
-  // };
+  // Muck with entries for hot reloading
+  WebpackDevServer.addDevServerEntrypoints(config, devServerOptions);
+  // Make localhost hot relaod if 0.0.0.0 is specified.
+  if (host === '0.0.0.0') {
+    WebpackDevServer.addDevServerEntrypoints(config, Object.assign({}, config.devServer, { host: 'localhost' }));
+  }
 
-  // serve(serveConfig);
-
+  // get a compiler
   const compiler = Webpack(config);
-
+  // get a server
   const devServer = new WebpackDevServer(compiler, devServerOptions);
 
+  // start that server.
   devServer.listen(port, host, () => {
+    // profit
     console.log(`Starting server on http://${host}:${port}`);
   });
 };
