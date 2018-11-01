4bbdac7e66089c61bba0339ee81ef7ace00d2828
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-11-01 18:05:35 -0500

    Be semantically correct according to commander docs

--- scripts/wdio/clean-screenshots-cli.js
+++ scripts/wdio/clean-screenshots-cli.js
@@ -8,7 +8,7 @@ const packageJson = require('../../package.json');
 // Parse process arguments
 commander
   .version(packageJson.version)
-  .option('--config <path>', 'The wdio config path for the tests', undefined)
+  .option('--config [path]', 'The wdio config path for the tests', undefined)
   .option('--removeReference', 'Whether or not to remove the reference screenshots', false)
   .parse(process.argv);
 
