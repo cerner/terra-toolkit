0aebd2d5892e45c18950e8984d423aec1c1d81b6
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2018-11-12 10:57:29 -0600

    parseInt is returning NaN for default ...

--- scripts/serve/serve-cli.js
+++ scripts/serve/serve-cli.js
@@ -9,13 +9,13 @@ commander
   .version(packageJson.version)
   .option('--config [path]', 'The webpack config to serve.', undefined)
   .option('--host [string]', 'Sets the host that the server will listen on. eg. \'10.10.10.1\'', '0.0.0.0')
-  .option('--port [number]', 'The port the server should listen on.', Number(parseInt), 8080)
+  .option('--port [number]', 'The port the server should listen on.', 8080)
   .option('-p, --production', 'Passes the -p flag to the webpack config')
   .parse(process.argv);
 
 serve({
   config: loadWebpackConfig(commander.config),
   host: commander.host,
-  port: commander.port,
+  port: Number(commander.port),
   production: commander.production,
 });
