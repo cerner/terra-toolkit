fc620a8a8623f5685f8c51de3a5be0a82a2481a1
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-12-13 15:41:21 -0600

    We do accept a fileSystem prop, so this is actually a bug...

--- tests/jest/aggregate-messages.test.js
+++ tests/jest/aggregate-messages.test.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const aggregateMessages = require('../../scripts/aggregate-translations/aggregate-messages');
 
@@ -8,6 +9,7 @@ const translationDirectories = [
   path.resolve(__dirname, 'fixtures', 'ned_modules', 'fixtures1', 'translations'),
 ];
 const locales = ['en', 'en-US', 'es'];
+const fileSystem = fs;
 
 const expectedMessages = {
   'Terra.fixtures.test': 'Test...',
@@ -16,7 +18,7 @@ const expectedMessages = {
 
 describe('aggregates translations messages', () => {
   it('returns empty messages when no translations directories are provided', () => {
-    const messages = aggregateMessages([], locales);
+    const messages = aggregateMessages([], locales, fileSystem);
 
     expect(messages).toHaveProperty('en', {});
     expect(messages).toHaveProperty('en-US', {});
@@ -24,14 +26,14 @@ describe('aggregates translations messages', () => {
   });
 
   it('logs a warning message if a translation file is not found', () => {
-    aggregateMessages([__dirname], ['en']);
+    aggregateMessages([__dirname], ['en'], fileSystem);
 
     // eslint-disable-next-line no-console
     expect(console.warn).toBeCalled();
   });
 
   it('aggregates the messages', () => {
-    const messages = aggregateMessages(translationDirectories, locales);
+    const messages = aggregateMessages(translationDirectories, locales, fileSystem);
 
     expect(messages).toHaveProperty('en', expectedMessages);
     expect(messages).toHaveProperty('en-US', expectedMessages);
