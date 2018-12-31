4a7b723e3602641b6c0ee0ae2f8f820ad38ea894
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-12-31 10:00:41 -0600

    update danger file to work

--- dangerfile.js
+++ dangerfile.js
@@ -1,44 +1,22 @@
-// eslint-disable-next-line import/no-extraneous-dependencies, no-unused-vars, object-curly-newline
-import { danger, warn, fail, message } from 'danger';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { danger, warn, fail } from 'danger';
 
-const newChangelog = danger.git.created_files.filter((filePath) => {
+const hasCHANGELOGChanges = danger.git.modified_files.some((filePath) => {
   const srcFilePattern = /CHANGELOG.md/i;
   return srcFilePattern.test(filePath);
 });
 
-const modifiedChangelog = danger.git.modified_files.filter((filePath) => {
-  const srcFilePattern = /CHANGELOG.md/i;
-  return srcFilePattern.test(filePath);
-});
-console.log('DAN...GER', danger.git.commits)
-
-const modifiedSrcFiles = danger.git.modified_files.filter((filePath) => {
-  const srcFilePattern = /\/src/i;
-  return srcFilePattern.test(filePath);
+const hasModifiedFiles = danger.git.modified_files.some((filePath) => {
+  const filePattern = /(src|scripts|config)/;
+  return !filePath.includes('md') && filePattern.test(filePath);
 });
 
-const modifiedSriptFiles = danger.git.modified_files.filter((filePath) => {
-  const scriptFilePattern = /\/scripts/i;
-  return scriptFilePattern.test(filePath);
-});
-
-const modifiedConfigFiles = danger.git.modified_files.filter((filePath) => {
-  const scriptFilePattern = /\/config/i;
-  return scriptFilePattern.test(filePath);
-});
-
-const hasCHANGELOGChanges = modifiedChangelog.length > 0 || newChangelog.length > 0;
-const hasModifiedSrcFiles = modifiedSrcFiles.length > 0;
-const hasModifiedScriptFiles = modifiedSriptFiles.length > 0;
-const hasModifiedConfigFiles = modifiedConfigFiles.length > 0;
-
-// Fail if there are src code changes without a CHANGELOG update
-if ((hasModifiedSrcFiles || hasModifiedScriptFiles || hasModifiedConfigFiles) && !hasCHANGELOGChanges) {
+if (hasModifiedFiles && !hasCHANGELOGChanges) {
   fail('Please include a CHANGELOG entry with this PR.');
 }
 
 // Warn when there is a big PR
 const bigPRThreshold = 1000;
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-  warn(':exclamation: Big PR. Consider breaking this into smaller PRs if applicaple');
+  warn(':exclamation: Big PR. Consider breaking this into smaller PRs if applicable');
 }
