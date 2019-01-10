e8685e94335bc7d6e981c3d7da3b6658c038e7e8
Manuel,Ryan <Ryan.Manuel@Cerner.com>
2019-01-10 12:55:46 -0600

    Refactoring comparisons

---
+++ src/wdio/services/compare/LocalScreenshotProcessor.js
@@ -0,0 +1,106 @@
+import path from 'path';
+import fs from 'fs-extra';
+import TerraCompare from './TerraCompare';
+import { terraViewports as VIEWPORTS } from '../../../../config/wdio/services.default-config';
+
+const screenshotSetup = {
+  diffDir: 'diff',
+  referenceDir: 'reference',
+  screenshotDir: 'latest',
+};
+
+const testIdRegex = /\[([^)]+)\]/;
+
+export default class LocalScreenshotProcessor extends TerraCompare {
+  static createTestName(fullName) {
+    const matches = testIdRegex.exec(fullName);
+
+    // If test ID is provided, use the ID for a shorter test name, otherwise use the full name
+    let name = matches ? matches[1] : fullName.trim();
+
+    // Remove white space
+    name = name.replace(/[\s+.]/g, '_');
+
+    // Remove windows reserved characters. See: https://msdn.microsoft.com/en-us/library/windows/desktop/aa365247(v=vs.85).aspx#naming_conventions
+    // eslint-disable-next-line no-useless-escape
+    name = name.replace(/[\?\<\>\/\\|\*\"\:\+\.]/g, '-');
+
+    return name;
+  }
+
+  static getScreenshotName(context) {
+    const parentName = LocalScreenshotProcessor.createTestName(context.test.parent);
+    const testName = LocalScreenshotProcessor.createTestName(context.test.title);
+
+    return `${parentName}[${testName}].png`;
+  }
+
+  static getFormFactor(context) {
+    let formFactor = global.browser.options.formFactor;
+
+    if (!formFactor) {
+      const browserWidth = context.meta.viewport.width;
+      const viewportSizes = Object.keys(VIEWPORTS);
+      viewportSizes.some((viewport) => {
+        if (browserWidth <= VIEWPORTS[viewport].width) {
+          formFactor = VIEWPORTS[viewport].name;
+          return true;
+        }
+        return false;
+      });
+    }
+
+    // If viewport value is undefined due to being larger then the terra defiend viewports, set formFactor to enormous.
+    formFactor = formFactor || 'enormous';
+
+    return formFactor;
+  }
+
+  static getScreenshotDir(context) {
+    const locale = global.browser.options.locale || 'en';
+    const browserName = context.desiredCapabilities.browserName;
+    const formFactor = LocalScreenshotProcessor.getFormFactor(context);
+    const testForm = `${browserName}_${formFactor}`;
+    const testSuite = path.parse(context.test.file).name;
+
+    return path.join(locale, testForm, testSuite);
+  }
+
+  static getScreenshotPath(ref, context) {
+    let testPath = path.dirname(context.test.file);
+
+    const baseDir = global.browser.options.baseScreenshotDir;
+    if (baseDir) {
+      testPath = testPath.split(process.cwd())[1];
+      // Added to allow for test reusablility from terra repositories
+      if (testPath.includes('node_modules')) {
+        testPath = testPath.split('node_modules')[1];
+      }
+      testPath = baseDir + testPath;
+    }
+    const refDir = screenshotSetup[`${ref}Dir`];
+
+    return path.join(testPath, '__snapshots__', refDir, LocalScreenshotProcessor.getScreenshotDir(context), LocalScreenshotProcessor.getScreenshotName(context));
+  }
+
+  static async getReferenceScreenshotData(context) {
+    const referenceScreenshotPath = LocalScreenshotProcessor.getScreenshotPath('reference', context);
+    return fs.readFile(referenceScreenshotPath);
+  }
+
+  static async saveLatestScreenshotData(context, latestScreenshotData) {
+    const latestScreenshotPath = LocalScreenshotProcessor.getScreenshotPath('screenshot', context);
+    await fs.outputFile(latestScreenshotPath, latestScreenshotData);
+  }
+
+  static async saveDiffFileData(context, diffData) {
+    const diffScreenshotPath = LocalScreenshotProcessor.getScreenshotPath('diff', context);
+    await fs.outputFile(diffScreenshotPath, diffData);
+  }
+
+  static async handleNoReferenceScreenshot(context, latestScreenshotData, createResultReport) {
+    const referenceScreenshotPath = LocalScreenshotProcessor.getScreenshotPath('reference', context);
+    await fs.outputFile(referenceScreenshotPath, latestScreenshotData);
+    return createResultReport(0, true, true);
+  }
+}
---
+++ src/wdio/services/compare/TerraCompare.js
@@ -0,0 +1,54 @@
+import BaseCompare from 'wdio-visual-regression-service/lib/methods/BaseCompare';
+import resemble from 'node-resemble-js';
+import get from 'lodash.get';
+
+export default class TerraCompare extends BaseCompare {
+  constructor(screenshotProcessor) {
+    super();
+    this.screenshotProcessor = screenshotProcessor;
+  }
+
+  async processScreenshot(context, base64Screenshot) {
+    const referenceScreenshotData = await this.screenshotProcessor.getReferenceScreenshotData(context);
+    const latestScreenshotData = Buffer.from(base64Screenshot, 'base64');
+    await this.screenshotProcessor.saveLatestScreenshotData(context, latestScreenshotData);
+    if (referenceScreenshotData) {
+      const comparisonData = await TerraCompare.compareImages(referenceScreenshotData, latestScreenshotData);
+      const { isSameDimensions } = comparisonData;
+      const misMatchPercentage = Number(comparisonData.misMatchPercentage);
+      const misMatchTolerance = get(context, 'options.misMatchTolerance', 0.01);
+
+      if (misMatchPercentage > misMatchTolerance) {
+        const png = comparisonData.getDiffImage().pack();
+        const diffImageData = await TerraCompare.createDiff(png);
+        await this.screenshotProcessor.saveDiffFileData(context, diffImageData);
+
+        return this.createResultReport(misMatchPercentage, false, isSameDimensions);
+      }
+      return this.createResultReport(misMatchPercentage, true, isSameDimensions);
+    }
+    return this.screenshotProcessor.handleNoReferenceScreenshot(context, latestScreenshotData, this.createResultReport);
+  }
+
+  static async compareImages(reference, screenshot) {
+    return new Promise((resolve) => {
+      const image = resemble(reference).compareTo(screenshot);
+      image.onComplete((data) => {
+        resolve(data);
+      });
+    });
+  }
+
+  static async createDiff(png) {
+    return new Promise((resolve, reject) => {
+      const chunks = [];
+      png.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+      png.on('end', () => {
+        resolve(Buffer.concat(chunks));
+      });
+      png.on('error', err => reject(err));
+    });
+  }
+}
