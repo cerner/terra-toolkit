e5356c59d8b2b7e31202b38cbc9ad573063cef56
Matt Henkes <mjhenkes@gmail.com>
2019-01-14 09:44:20 -0600

    Making echo more descriptive.

--- docker/terra-node/entrypoint/npm_install.sh
+++ docker/terra-node/entrypoint/npm_install.sh
@@ -2,7 +2,7 @@
 # check if the mode modules folder exists and is populated.
 if [ -n "$(ls -A node_modules 2>/dev/null)" ]
 then
-  echo "node_modules installed"
+  echo "Local node_modules exists. Copying local node_modules to docker container."
 else
   npm install
 fi
