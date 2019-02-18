966f579fa60c6bb69df57cb81adc73af6989452e
Manuel,Ryan <Ryan.Manuel@Cerner.com>
2019-02-18 11:37:33 -0600

    Update to enable accessibility testing on screenshots

--- tests/wdio/matchScreenshotAndTestStandards-spec.js
+++ tests/wdio/matchScreenshotAndTestStandards-spec.js
@@ -1,9 +1,18 @@
-/* global describe, it, expect, Terra */
+/* global describe, before, browser, it, expect, Terra */
 
 const accessibility = require('../../lib/wdio/services/TerraCommands/accessiblity').default;
 const visualRegressions = require('../../lib/wdio/services/TerraCommands/visual-regression').default;
 
 describe('matchScreenshotAndTestStandards', () => {
+  describe('full implementation', () => {
+    before(() => {
+      browser.url('/compare.html');
+      browser.setViewportSize(Terra.viewports('tiny')[0]);
+    });
+
+    Terra.should.matchScreenshotAndTestStandards();
+  });
+
   it('calls the appropriate methods downstream with a name', () => {
     const oldAccessible = accessibility.beAccessible;
 
