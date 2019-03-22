3433b038527e132f55e97678681a09754bb56737
Naveen Kumar Ramamurthy <nramamur@buffalo.edu>
2019-03-22 09:54:14 +0530

    Added defaults to the CLI help text.

--- scripts/aggregate-translations/aggregate-translations-cli.js
+++ scripts/aggregate-translations/aggregate-translations-cli.js
@@ -1,5 +1,6 @@
 const commander = require('commander');
 const i18nPackageJson = require('../../package.json');
+const supportedLocales = require('./i18nSupportedLocales');
 const parseCLIList = require('../utils/parse-cli-list');
 
 const aggregateTranslations = require('./aggregate-translations');
@@ -7,13 +8,13 @@ const aggregateTranslations = require('./aggregate-translations');
 // Parse process arguments
 commander
   .version(i18nPackageJson.version)
-  .option('-b, --baseDir [baseDir]', 'The directory to start searching from and to prepend to the output directory')
-  .option('-d, --directories [directories]', 'Regex pattern to glob search for translations', parseCLIList)
-  .option('-l, --locales [locales]', 'The list of locale codes aggregate on and combine into a single, respective translation file ', parseCLIList)
-  .option('-o, --outputDir [outputDir]', 'The output location of the generated configuration file')
-  .option('-c, --config [configPath]', 'The path to the terra i18n configuration file')
-  .option('-e, --exclude [exclude]', 'Regex pattern to glob filter out directories', parseCLIList)
-  .option('-f, --format [format]', 'Format to output the aggregated translations to. Options are [es5, es6]', 'es5')
+  .option('-b, --baseDir [baseDir]', `The directory to start searching from and to prepend to the output directory. Default: ${process.cwd()}`)
+  .option('-d, --directories [directories]', 'Regex pattern to glob search for translations. Default: []', parseCLIList)
+  .option('-l, --locales [locales]', `The list of locale codes aggregate on and combine into a single, respective translation file. Default: ${supportedLocales}`, parseCLIList)
+  .option('-o, --outputDir [outputDir]', 'The output location of the generated configuration file. Default: ./aggregated-translations')
+  .option('-c, --config [configPath]', 'The path to the terra i18n configuration file.')
+  .option('-e, --exclude [exclude]', 'Regex pattern to glob filter out directories. Default: []', parseCLIList)
+  .option('-f, --format [format]', 'Format to output the aggregated translations to. Options are [es5, es6]. Default: es5')
   .parse(process.argv);
 
 const aggregationOption = {
