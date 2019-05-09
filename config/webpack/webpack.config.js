2ad089f0f715614ae48b70e8292305d0cb13198e
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2019-05-09 15:39:44 -0500

    Autoprefixer does not recommend directly passing

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -6,7 +6,6 @@ const rtl = require('postcss-rtl');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const browserslist = require('browserslist-config-terra');
 const merge = require('webpack-merge');
 const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
 const aggregateTranslations = require('../../scripts/aggregate-translations/aggregate-translations');
@@ -63,7 +62,7 @@ const webpackConfig = (options, env, argv) => {
                 plugins() {
                   return [
                     rtl(),
-                    Autoprefixer({ browsers: browserslist }),
+                    Autoprefixer(),
                   ];
                 },
               },
