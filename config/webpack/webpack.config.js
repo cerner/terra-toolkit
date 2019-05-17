01095d627aa881e0828c2da259d365ab8d7db849
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2019-05-17 09:28:42 -0500

    fix

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const browserslist = require('browserslist-config-terra');
 const merge = require('webpack-merge');
 const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
-const aggregateTranslations = require('terra-aggregate-translations');
+const aggregateTranslations = require('../../scripts/aggregate-translations/aggregate-translations');
 const ThemeAggregator = require('../../scripts/aggregate-themes/theme-aggregator');
 
 const webpackConfig = (options, env, argv) => {
