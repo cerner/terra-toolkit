3c957891b66449a68a64017894345ee7aaa751d4
Matt Henkes <mjhenkes@gmail.com>
2019-05-27 21:51:11 -0500

    fix jest test

--- tests/jest/validateElement.test.js
+++ tests/jest/validateElement.test.js
@@ -1,19 +1,19 @@
 jest.mock('../../lib/wdio/services/TerraCommands/accessibility', () => ({
   __esModule: true,
   default: {
-    accessibleItBlock: jest.fn(),
+    runAccessibilityTest: jest.fn(),
   },
 }));
 jest.mock('../../lib/wdio/services/TerraCommands/visual-regression', () => ({
   __esModule: true,
   default: {
-    screenshotItBlock: jest.fn(),
+    runMatchScreenshotTest: jest.fn(),
   },
 }));
 
 const accessibility = require('../../lib/wdio/services/TerraCommands/accessibility').default;
 const visualRegressions = require('../../lib/wdio/services/TerraCommands/visual-regression').default;
-const validateElement = require('../../lib/wdio/services/TerraCommands/validate-element').default;
+const { validatesElement } = require('../../lib/wdio/services/TerraCommands/validate-element').default;
 
 global.browser = {
   options: {
@@ -25,23 +25,23 @@ global.browser = {
 
 describe('validateElement', () => {
   it('calls the appropriate methods downstream with all arguments', () => {
-    validateElement('test name', { selector: 'test-selector', misMatchTolerance: 0.05, axeRules: { a: 'b', c: 'd' } });
+    validatesElement('test name', { selector: 'test-selector', misMatchTolerance: 0.05, axeRules: { a: 'b', c: 'd' } });
 
-    expect(visualRegressions.screenshotItBlock).toBeCalledWith('test name', 'test-selector', { misMatchTolerance: 0.05 });
-    expect(accessibility.accessibleItBlock).toBeCalledWith({ rules: { a: 'b', c: 'd' } });
+    expect(visualRegressions.runMatchScreenshotTest).toBeCalledWith('test-selector', { misMatchTolerance: 0.05, name: 'test name' });
+    expect(accessibility.runAccessibilityTest).toBeCalledWith({ rules: { a: 'b', c: 'd' } });
   });
 
   it('calls the appropriate methods downstream with defaults', () => {
-    validateElement();
+    validatesElement();
 
-    expect(visualRegressions.screenshotItBlock).toBeCalledWith('default', '[data-terra-toolkit-content]', { });
-    expect(accessibility.accessibleItBlock).toBeCalled();
+    expect(visualRegressions.runMatchScreenshotTest).toBeCalledWith('[data-terra-toolkit-content]', { name: 'default' });
+    expect(accessibility.runAccessibilityTest).toBeCalled();
   });
 
   it('does not use context option', () => {
-    validateElement({ selector: 'test-selector', context: 'im_sneaky_and_will_test_a_diff_element_for_a11y' });
+    validatesElement({ selector: 'test-selector', context: 'im_sneaky_and_will_test_a_diff_element_for_a11y' });
 
-    expect(visualRegressions.screenshotItBlock).toBeCalledWith('default', 'test-selector', { });
-    expect(accessibility.accessibleItBlock).toBeCalled();
+    expect(visualRegressions.runMatchScreenshotTest).toBeCalledWith('test-selector', { name: 'default' });
+    expect(accessibility.runAccessibilityTest).toBeCalled();
   });
 });
