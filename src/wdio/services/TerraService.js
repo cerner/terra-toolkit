102785a03334a9f71bec7e7124fd687f3ebfe83a
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2019-05-30 14:26:39 -0500

    fix missed merge conflict

--- src/wdio/services/TerraService.js
+++ src/wdio/services/TerraService.js
@@ -1,11 +1,7 @@
 import chai from 'chai';
 import axeCommand from './TerraCommands/axe-command';
 import chaiMethods from './TerraCommands/chai-methods';
-<<<<<<< HEAD
 import accessibility from './TerraCommands/accessibility';
-=======
-import accessibility from './TerraCommands/accessiblity';
->>>>>>> master
 import visualRegression from './TerraCommands/visual-regression';
 import validateElement from './TerraCommands/validate-element';
 import viewportHelpers from './TerraCommands/viewport-helpers';
@@ -24,14 +20,10 @@ export default class TerraService {
   }
 
   // eslint-disable-next-line class-methods-use-this
-<<<<<<< HEAD
   before(capabilities) {
     /* Add Terra's custom Wdio command for a11y testing. */
     global.browser.addCommand('axe', axeCommand);
 
-=======
-  before() {
->>>>>>> master
     /* Initialize the Chai assertion library to have more expressive tests. */
     chai.config.showDiff = false;
     global.expect = chai.expect;
@@ -49,25 +41,9 @@ export default class TerraService {
       /* `describeViewports` provides a custom Mocha `describe` block for looping test viewports. */
       describeViewports: viewportHelpers.describeViewports,
 
-<<<<<<< HEAD
       /* `validates` provides access to the chai assertions to use in Mocha `it` blocks. */
       validates: {
         accessibility: accessibility.validatesAccessibility,
-=======
-
-      /* `should` provides access to Mocha it blocks of test assertions. */
-      should: {
-        beAccessible: accessibility.itIsAccessible,
-        matchScreenshot: visualRegression.itMatchesScreenshot,
-        themeEachCustomProperty: visualRegression.themeEachCustomProperty,
-        themeCombinationOfCustomProperties: visualRegression.themeCombinationOfCustomProperties,
-        validateElement: validateElement.itValidatesElement,
-      },
-
-      /* `validates` provides access to the chai assertions to use in Mocha `it` blocks. */
-      validates: {
-        accessibility: accessibility.runAccessibilityTest,
->>>>>>> master
         screenshot: visualRegression.validatesScreenshot,
         element: validateElement.validatesElement,
       },
@@ -81,25 +57,11 @@ export default class TerraService {
     };
 
     /* IE driver takes a longer to be ready for browser interactions. */
-<<<<<<< HEAD
     if (capabilities.browserName === 'internet explorer') {
-=======
-    if (global.browser.desiredCapabilities.browserName === 'internet explorer') {
->>>>>>> master
       global.browser.pause(10000);
     }
 
     /* Set the viewport size before the spec begins.  */
     viewportHelpers.setViewport(global.browser.options.formFactor);
-<<<<<<< HEAD
-=======
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  onComplete() {
-    Logger.log(`${Logger.emphasis('Be proactive for toolkit v5!')} Update Terra.should.matchScreenshot to Terra.it.matchesScreenshot.`, { context: '[Terra-Toolkit:terra-service]' });
-    Logger.log(`${Logger.emphasis('Be proactive for toolkit v5!')} Update Terra.should.beAccessible to Terra.it.isAccessible.`, { context: '[Terra-Toolkit:terra-service]' });
-    Logger.log(`${Logger.emphasis('Be proactive for toolkit v5!')} Update Terra.should.validateElement to Terra.it.validatesElement.`, { context: '[Terra-Toolkit:terra-service]' });
->>>>>>> master
   }
 }
