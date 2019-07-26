e56a2ef1c493660b497bb9277b3d6ad06eca668e
Nathan Faltermeier <nathan.faltermeier@cerner.com>
2019-07-26 08:28:51 -0500

    Update test structure

--- tests/wdio/hideInputCaret-spec.js
+++ tests/wdio/hideInputCaret-spec.js
@@ -1,30 +1,43 @@
 describe('hideInputCaret', () => {
   before(() => {
     browser.url('/input.html');
-    // The input explicitly has the caret-color set to black
-    Terra.hideInputCaret('#inputID');
   });
 
-  it('validates the input\'s caret-color is transparent', () => {
-    const element = browser.element('#inputID');
+  it('validates the body\'s caret-color is transparent by default', () => {
+    const element = browser.element('#textareaID');
+
     // wdio parses colors with rgb2hex so it becomes a rgba value
     // http://v4.webdriver.io/api/property/getCssProperty.html
     expect(element.getCssProperty('caretColor').value).to.equal('rgba(0,0,0,0)');
   });
 
-  it('validates the textarea\'s caret-color is transparent', () => {
+  it('validates the textarea\'s caret-color is inherited as transparent', () => {
     const element = browser.element('#textareaID');
-    // Terra service automatically sets caretColor on the body to transparent which gets inherited
+
     expect(element.getCssProperty('caretColor').value).to.equal('rgba(0,0,0,0)');
   });
 
-  it('validates the body\'s caret-color is transparent', () => {
-    const element = browser.element('#textareaID');
-    // Terra service automatically sets caretColor on the body to transparent
+  it('validates the input\'s caret-color is inherited as transparent', () => {
+    const element = browser.element('#inputID');
+
     expect(element.getCssProperty('caretColor').value).to.equal('rgba(0,0,0,0)');
   });
 
-  it('hides the input caret on a non-existent element', () => {
+  it('sets the input\'s caret-color to orange', () => {
+    browser.execute('document.querySelector("#inputID").style.caretColor = "rgb(255, 165, 0)"');
+    const element = browser.element('#inputID');
+
+    expect(element.getCssProperty('caretColor').value).to.equal('rgb(255,165,0)');
+  });
+
+  it('sets the input\'s caret-color back to transparent', () => {
+    Terra.hideInputCaret('#inputID');
+    const element = browser.element('#inputID');
+
+    expect(element.getCssProperty('caretColor').value).to.equal('rgba(0,0,0,0)');
+  });
+
+  it('throws an error for a non-existent element', () => {
     expect(Terra.hideInputCaret.bind(this, '#NotAnElement')).to.throw('No element could be found');
   });
 });
