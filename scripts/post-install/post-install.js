db62c49e57d3e47abaf6ac479fa8fc5e0f042fe9
Stephen Esser <Stephen.Esser@Cerner.com>
2019-09-03 11:01:39 -0500

    Update warning message

--- scripts/post-install/post-install.js
+++ scripts/post-install/post-install.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
 const Logger = require('../utils/logger');
 
-fs.readFile(`${process.cwd()}/package.json`, (error, file) => {
+fs.readFile(`${process.env.INIT_CWD}/package.json`, (error, file) => {
   if (error) {
-    Logger.error(error);
+    throw Logger.error(error);
   }
-  console.log(JSON.parse(file));
+
+  const { dependencies } = JSON.parse(file);
+
+  if (dependencies['terra-toolkit']) {
+    Logger.warn('+-------------------------------------------------------------------------------------------------------------+');
+    Logger.warn('|                                                   WARNING                                                   |');
+    Logger.warn('+-------------------------------------------------------------------------------------------------------------+');
+    Logger.warn('|                            terra-toolkit should be installed as a devDependency.                            |');
+    Logger.warn('|                                                                                                             |');
+    Logger.warn('| Modify the package.json to remove terra-toolkit from dependencies and add terra-toolkit to devDependencies. |');
+    Logger.warn('+-------------------------------------------------------------------------------------------------------------+');
+  }
 });
