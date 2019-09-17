311cb376478d359231474375ed1c67b9f1e0fd36
Ryan Manuel <rfmanuel@gmail.com>
2019-09-16 19:27:26 -0500

    Update scripts/aggregate-themes/theme-aggregator.js

--- scripts/aggregate-themes/theme-aggregator.js
+++ scripts/aggregate-themes/theme-aggregator.js
@@ -129,7 +129,7 @@ class ThemeAggregator {
     }
 
     let themesToAggregate = defaultTheme ? [defaultTheme] : [];
-    if (scoped.length) {
+    if (scoped) {
       themesToAggregate = themesToAggregate.concat(scoped);
     }
 
