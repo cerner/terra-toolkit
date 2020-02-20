ad0d5a3a1142394aa70d71e23bc8ca8377b56bec
Matt Henkes <mjhenkes@gmail.com>
2020-02-20 14:30:09 -0600

    update plugin

--- scripts/postcss/ThemePlugin.js
+++ scripts/postcss/ThemePlugin.js
@@ -1,7 +1,26 @@
+const fs = require('fs-extra');
+const path = require('path');
 const postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-test-plugin', () => (root) => {
-  root.walk((node) => {
-    console.log(node.toString);
-  });
+const CONFIG = 'terra-theme.config.js';
+
+module.exports = postcss.plugin('postcss-test-plugin', () => {
+  let themeConfig = {};
+  const defaultConfig = path.resolve(process.cwd(), CONFIG);
+  if (fs.existsSync(defaultConfig)) {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    themeConfig = require(defaultConfig);
+  }
+  const defaultThemeSelector = `.${themeConfig.theme}`;
+
+  return (root) => {
+    if (defaultThemeSelector) {
+      root.walk((decl) => {
+        if (decl.selector === defaultThemeSelector) {
+          const clone = decl.clone({ selector: ':root' });
+          root.append(clone);
+        }
+      });
+    }
+  };
 });
