4dbb4bc58c06ed2ae189256763739b628df0b532
Matt Henkes <mjhenkes@gmail.com>
2020-04-06 13:48:06 -0500

    update plugin name

--- config/webpack/postcss/ThemePlugin.js
+++ config/webpack/postcss/ThemePlugin.js
@@ -14,7 +14,7 @@ const SUPPORTED_THEMES = [
  * The purpose of this plugin is to create a default theme from a scoped theme
  * and to remove any supported themes that are not desired.
  */
-module.exports = postcss.plugin('postcss-test-plugin', (config) => {
+module.exports = postcss.plugin('terra-theme-plugin', (config) => {
   // Retrieve theme config.
   let themeConfig = {};
   if (config) {
