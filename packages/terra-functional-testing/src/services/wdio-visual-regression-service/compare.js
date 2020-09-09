21c4f0a7289b58d31f980ced4f640d4cc9afcdad
Emily Rohrbough <emily.rohrbough@cerner.com>
2020-09-09 14:42:59 -0500

    remove spectre support

--- packages/terra-functional-testing/src/services/wdio-visual-regression-service/compare.js
+++ packages/terra-functional-testing/src/services/wdio-visual-regression-service/compare.js
@@ -1,3 +1,2 @@
 export { default as LocalCompare } from './methods/LocalCompare';
 export { default as SaveScreenshot } from './methods/SaveScreenshot';
-export { default as Spectre } from './methods/Spectre';
