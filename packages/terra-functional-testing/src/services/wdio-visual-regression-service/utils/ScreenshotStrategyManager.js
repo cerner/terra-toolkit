380b36d03425b0a298ac458db8fb91da5a5c6911
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2020-09-10 15:07:27 -0500

    [terra-functional-testing] Remove phantomjs support (#433)
    * Remove PhantomJS support
    
    * change log
    
    * Update CHANGELOG.md

--- packages/terra-functional-testing/src/services/wdio-visual-regression-service/utils/ScreenshotStrategyManager.js
+++ packages/terra-functional-testing/src/services/wdio-visual-regression-service/utils/ScreenshotStrategyManager.js
@@ -2,32 +2,11 @@ import logger from '@wdio/logger';
 
 import MergeViewportStrategy from './strategies/MergeScreenshotStrategy';
 import TrimAndMergeViewportStrategy from './strategies/TrimAndMergeScreenshotStrategy';
-import FullpageScreenshotStrategy from './strategies/FullpageScreenshotStrategy';
-
-const regexFirefox = /firefox/i; // eslint-disable-line
-const regexPhantomjs = /phantomjs/i;
 
 const log = logger('wdio-visual-regression-service:ScreenshotStrategyManager');
 
-function matchBrowserName(browser, regex) {
-  return (
-    browser.desiredCapabilities
-    && browser.desiredCapabilities.browserName
-    && regex.test(browser.desiredCapabilities.browserName)
-  );
-}
-
-function isPhantomjs(browser) {
-  return matchBrowserName(browser, regexPhantomjs);
-}
-
 export default class ScreenshotStrategyManager {
   static getStrategy(browser, screenDimensions) {
-    if (isPhantomjs(browser)) {
-      log.info('use full page strategy');
-      return new FullpageScreenshotStrategy(screenDimensions);
-    }
-
     const { isIOS } = browser;
     if (isIOS) {
       log.info('use iOS Trim and Merge viewport strategy');
