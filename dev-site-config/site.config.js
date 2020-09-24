d676fe5631d8af3e61536c7dd62ea673224223f4
Matt Henkes <mjhenkes@gmail.com>
2020-09-24 14:18:15 -0500

    Add changes for hosting a terra-dev-site docs page

---
+++ dev-site-config/site.config.js
@@ -0,0 +1,15 @@
+const navConfig = require('./navigation.config');
+
+const siteConfig = {
+  appConfig: {
+    themes: {
+      'Default Theme': '',
+      'Clinical Low Light Theme': 'clinical-lowlight-theme',
+      'Orion Fusion Theme': 'orion-fusion-theme',
+    },
+  },
+  navConfig,
+  includeTestEvidence: false,
+};
+
+module.exports = siteConfig;
---
+++ webpack.dev-site.config.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const merge = require('webpack-merge');
+const {
+  TerraDevSite,
+  TerraDevSiteEntrypoints,
+  DirectorySwitcherPlugin,
+  LocalPackageAliasPlugin,
+} = require('terra-dev-site');
+
+const WebpackConfigTerra = require('./packages/webpack-config-terra/webpack.config');
+/**
+* Generates the file representing app name configuration.
+*/
+const devSiteConfig = (env = {}, argv = {}) => {
+  const production = argv.p;
+  return {
+    entry: TerraDevSiteEntrypoints,
+    plugins: [
+      new TerraDevSite({ env }),
+    ],
+    resolve: {
+      plugins: [
+        new DirectorySwitcherPlugin({
+          shouldSwitch: !production,
+        }),
+        new LocalPackageAliasPlugin({}),
+      ],
+    },
+  };
+};
+const webpackConfig = (env, argv) => (
+  merge(WebpackConfigTerra(env, argv), devSiteConfig(env, argv))
+);
+module.exports = webpackConfig;
