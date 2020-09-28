d48d37b93ad427f93c74d82586e722398aa89a87
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2020-09-28 15:37:46 -0500

    Update BaseCompare.js

--- packages/terra-functional-testing/src/services/wdio-visual-regression-service/methods/BaseCompare.js
+++ packages/terra-functional-testing/src/services/wdio-visual-regression-service/methods/BaseCompare.js
@@ -7,37 +7,7 @@ const log = logger('wdio-visual-regression-service:BaseCompare');
 const runtimeConfigPath = __dirname;
 
 export default class BaseCompare {
-  constructor() {
-    this.configs = new Map();
-  }
-
-  /**
-   * Gets executed once before all workers get launched.
-   */
-  async onPrepare() {
-    return Promise.resolve();
-  }
-
-  /**
-   * Gets executed before the tests starts.
-   */
-  async before(context) {
-    return Promise.resolve();
-  }
-
-  /**
-   * Gets executed immediately before the screenshot is taken.
-   */
-  async beforeScreenshot(context) {
-    return Promise.resolve();
-  }
-
-  /**
-   * Gets executed after the screenshot is taken.
-   */
-  async afterScreenshot(context, base64Screenshot) {
-    return Promise.resolve();
-  }
+  constructor() {}
 
   /**
    * You can do here your image comparison magic.
