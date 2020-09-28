ba89337342945a84c27900ff162da9611c26d4cf
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2020-09-28 16:00:50 -0500

    Update BaseCompare.js

--- packages/terra-functional-testing/src/services/wdio-visual-regression-service/methods/BaseCompare.js
+++ packages/terra-functional-testing/src/services/wdio-visual-regression-service/methods/BaseCompare.js
@@ -1,10 +1,4 @@
 /* eslint-disable class-methods-use-this, no-unused-vars */
-import fs from 'fs-extra';
-import path from 'path';
-import logger from '@wdio/logger';
-
-const log = logger('wdio-visual-regression-service:BaseCompare');
-const runtimeConfigPath = __dirname;
 
 export default class BaseCompare {
   /**
