825dac196d8e70c66767ce89f6479d4e73a75951
Cai,Ben <BCAI@CERNER.COM>
2020-10-28 17:44:15 -0500

    Check for selector in hide-input-caret.

--- packages/terra-functional-testing/src/commands/hide-input-caret.js
+++ packages/terra-functional-testing/src/commands/hide-input-caret.js
@@ -1,22 +1,16 @@
 const Logger = require('../logger/logger');
 
-const logger = new Logger({ prefix: 'wdio-terra-service' });
-
 /**
  * Hides the blinking input caret that appears within editable text areas to prevent inconsistent test failures.
  *
  * @param {string} selector The selector for the element to hide the caret of
  */
 const hideInputCaret = (selector) => {
-  try {
+  if (global.browser.$(selector).isExisting()) {
     global.browser.execute(`document.querySelector("${selector.replace(/"/g, '\\"')}").style.caretColor = "transparent";`);
-  } catch (error) {
-    const element = global.browser.$(selector);
-    if (!element.isExisting()) {
-      logger.error(`No element could be found with the selector '${selector}'.`);
-    } else {
-      throw error;
-    }
+  } else {
+    const logger = new Logger({ prefix: 'wdio-terra-service' });
+    logger.error(`No element could be found with the selector '${selector}'.`);
   }
 };
 
