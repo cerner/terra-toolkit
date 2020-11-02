b2f59a501db3af8d9397c201246b73563f85b78d
Emily Rohrbough <emily.rohrbough@cerner.com>
2020-11-02 10:43:03 -0600

    update viewports import location to match latest merge into main

--- packages/terra-functional-testing/src/services/wdio-visual-regression-service/modules/getTerraFormFactor.js
+++ packages/terra-functional-testing/src/services/wdio-visual-regression-service/modules/getTerraFormFactor.js
@@ -1,5 +1,4 @@
-import terraViewports from '../../../utils/viewports';
-
+import { TERRA_VIEWPORTS } from '../../../static-assets/viewports';
 /**
  * Determines the Terra form factor to for the current viewport size.
  *
@@ -10,10 +9,10 @@ export default function getTerraFormFactor(viewpointWidth) {
   // Default to enormous then check if the current viewport is a small form factor
   let formFactor = 'enormous';
 
-  const viewportSizes = Object.keys(terraViewports);
+  const viewportSizes = Object.keys(TERRA_VIEWPORTS);
   for (let form = 0; form < viewportSizes.length; form += 1) {
     const viewportName = viewportSizes[form];
-    if (viewpointWidth <= terraViewports[viewportName].width) {
+    if (viewpointWidth <= TERRA_VIEWPORTS[viewportName].width) {
       formFactor = viewportName;
       break;
     }
