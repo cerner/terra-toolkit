026c8e8907244e6f11774a2f1b8a5b18b10cec2d
Manuel,Ryan <Ryan.Manuel@Cerner.com>
2020-11-12 11:45:28 -0600

    Remove debug code

--- packages/terra-open-source-scripts/src/terra-cli/prepare-for-release/prepareForReleaseMonoRepoHandler.js
+++ packages/terra-open-source-scripts/src/terra-cli/prepare-for-release/prepareForReleaseMonoRepoHandler.js
@@ -20,19 +20,18 @@ module.exports = async () => {
     // Utilize inherit in terms of stdio so that we can interact with the user running the command to capture requested versions.
     // Exec doesn't allow using inherit and when you use inherit the output doesn't get returned. Thus, we use script
     // to capture the output to a file
-    const x = childProcess.execSync(`script -q ${VERSION_OUTPUT_PATH} lerna version --no-git-tag-version`, { stdio: 'inherit' });
-    console.log(x);
+    childProcess.execSync(`script -q ${VERSION_OUTPUT_PATH} lerna version --no-git-tag-version`, { stdio: 'inherit' });
 
-    // if (stripAnsi(await fs.readFile(VERSION_OUTPUT_PATH, 'utf-8')).includes('lerna success version finished')) {
-    //   const { stdout } = await exec('npx lerna changed'); // Clean up lerna updated output and convert to an array
-    //   const updatedPackages = stdout.split('\n').map(x => `packages/${x.replace('@cerner/', '')}`);
-    //   updatedPackages.pop(); // Remove last item as it is an empty string
+    if (stripAnsi(await fs.readFile(VERSION_OUTPUT_PATH, 'utf-8')).includes('lerna success version finished')) {
+      const { stdout } = await exec('npx lerna changed'); // Clean up lerna updated output and convert to an array
+      const updatedPackages = stdout.split('\n').map(x => `packages/${x.replace('@cerner/', '')}`);
+      updatedPackages.pop(); // Remove last item as it is an empty string
 
-    //   // Update release version in changelog files
-    //   await Promise.all(updatedPackages.map(packagePath => updateChangelogForPackage(packagePath)));
-    // } else {
-    //   logger.warn('Lerna version aborted');
-    // }
+      // Update release version in changelog files
+      await Promise.all(updatedPackages.map(packagePath => updateChangelogForPackage(packagePath)));
+    } else {
+      logger.warn('Lerna version aborted');
+    }
   } finally {
     fs.remove(VERSION_OUTPUT_PATH);
   }
