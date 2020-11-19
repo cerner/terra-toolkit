ebc8b0446c424f52dcdb92f91f4c0dfd08cdf4b3
Ryan Manuel <rfmanuel@gmail.com>
2020-11-18 20:22:42 -0600

    Fix string and spawn usage

--- packages/terra-open-source-scripts/src/terra-cli/release/setupGit.js
+++ packages/terra-open-source-scripts/src/terra-cli/release/setupGit.js
@@ -7,8 +7,8 @@ module.exports = async () => {
   const travis = process.env.TRAVIS;
 
   if (travis) {
-    await spawn('git', ['config', '--global', 'user.email', '"travis@travis-ci.org"'], { stdioString: true });
-    await spawn('git', ['config', '--global', 'user.name', '"Travis CI"'], { stdioString: true });
+    await spawn('git', ['config', '--global', 'user.email', 'travis@travis-ci.org'], { stdioString: true });
+    await spawn('git', ['config', '--global', 'user.name', 'Travis CI'], { stdioString: true });
     const remoteUrl = (await spawn('git', ['config', '--get', 'remote.origin.url'], { stdioString: true })).stdout.trim();
     const token = process.env.GITHUB_TOKEN;
     const newUrl = remoteUrl.replace('https://', `https://${token}@`);
