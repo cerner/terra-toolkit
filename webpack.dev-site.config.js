0bdb74f4d117fccdf139b2def821abc88875ae2d
Matt Henkes <mjhenkes@gmail.com>
2020-11-24 11:17:49 -0600

    Update webpack-config-terra dependencies (#489)
    * Update dependencies
    
    * more updates
    
    * sort
    
    * change log
    
    * remove post experiment
    
    * docs changelog

--- webpack.dev-site.config.js
+++ webpack.dev-site.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const path = require('path');
 const {
   TerraDevSite,
