decc90ac5d8a91e5a9a04d6d2a3699c5a6400ada
Stephen Esser <stephen.esser@cerner.com>
2020-12-03 14:29:18 -0600

    Restructure utility methods

---
+++ packages/terra-functional-testing/src/commands/utils/getViewports.js
@@ -0,0 +1,21 @@
+const { TERRA_VIEWPORTS } = require('../../constants');
+
+const ALL_VIEWPORTS = Object.keys(TERRA_VIEWPORTS);
+
+/**
+ * Returns an array of viewport dimensions.
+ * Each supported viewport has a width, height, and name.
+ * @param {string|string[]} sizes - A list of Terra viewport sizes.
+ * @return {array} - An array of viewports. Returns all viewports if sizes are not provided.
+ */
+const getViewports = (...sizes) => {
+  const viewports = sizes.length > 0 ? sizes : ALL_VIEWPORTS;
+
+  return viewports.map((name) => {
+    const { height, width } = TERRA_VIEWPORTS[name];
+
+    return { height, width, name };
+  });
+};
+
+module.exports = getViewports;
--- packages/terra-functional-testing/src/services/wdio-terra-service.js
+++ packages/terra-functional-testing/src/services/wdio-terra-service.js
@@ -1,42 +1,69 @@
 /* eslint-disable class-methods-use-this */
 const expect = require('expect');
-const { accessibility } = require('../commands/validates');
+const { accessibility, element } = require('../commands/validates');
 const { toBeAccessible } = require('../commands/expect');
-const { getViewports, describeViewports, setViewport } = require('../commands/viewport-helpers');
-
-const hideInputCaret = require('../commands/hide-input-caret');
-const { element } = require('../commands/validates');
+const {
+  describeViewports,
+  getViewports,
+  hideInputCaret,
+  setViewport,
+} = require('../commands/utils');
 
 class TerraService {
   constructor(options = {}) {
-    this.serviceOptions = {
-      theme: 'terra-default-theme',
-      ...options,
-    };
+    this.options = options;
   }
 
+  /**
+   * Service hook executed before worker processes are launched.
+   * @param {object} config - The WebdriverIO configuration object.
+   */
   onPrepare(config) {
-    // Add the configurations at the top level service options to the Terra Service options.
-    this.serviceOptions = {
+    const { serviceOptions } = config;
+
+    this.options = {
+      theme: 'terra-default-theme',
       selector: '[data-terra-test-content] *:first-child',
-      ...this.serviceOptions,
-      ...config.serviceOptions,
+      ...this.options,
+      ...serviceOptions,
+    };
+
+    /**
+     * Global axe override options.
+     * Options modified here will be applied globally for all tests.
+     */
+    this.axeOptions = {
+      rules: {
+        /**
+         * This rule was introduced in axe-core v3.3 and causes failures in many Terra components.
+         * The solution to address this failure vary by component. It is being disabled until a solution is identified in the future.
+         *
+         * Reference: https://github.com/cerner/terra-framework/issues/991
+         */
+        'scrollable-region-focusable': { enabled: false },
+        /**
+         * The lowlight theme adheres to a non-default color contrast ratio and fails the default ratio check.
+         * The color-contrast ratio check is disabled for lowlight theme testing.
+         */
+        'color-contrast': { enabled: this.options.theme !== 'clinical-lowlight-theme' },
+      },
     };
   }
 
+  /**
+   * Service hook executed prior to initializing the webdriver session.
+   */
   beforeSession() {
-    global.Terra = {
-      /*
-       This command must be defined in the beforeSession hook instead of together with the other Terra custom commands in the
-       before hook. The reason being webdriverio v6 now executed the describe block prior to running the before hook.
-       Therefore, this command needs to be defined before the test starts in the testing life cycle.
+    global.Terra = {};
 
-       Reference: https://github.com/webdriverio/webdriverio/issues/6119
-      */
-      describeViewports,
-
-      serviceOptions: this.serviceOptions,
-    };
+    /**
+     * This command must be defined in the beforeSession hook instead of together with the other Terra custom commands in the
+     * before hook. The reason being WebdriverIO v6 now executes the describe block prior to running the before hook.
+     * Therefore, this command needs to be defined before the test starts in the testing life cycle.
+     *
+     * Reference: https://github.com/webdriverio/webdriverio/issues/6119
+     */
+    global.Terra.describeViewports = describeViewports.bind(null, this.options.formFactor);
   }
 
   /**
@@ -48,43 +75,16 @@ class TerraService {
     global.expect = expect;
     global.expect.extend({ toBeAccessible });
 
-    // Add a Terra global with access to Mocha-Chai test helpers.
-    global.Terra = {
-      ...global.Terra,
+    // Setup and expose global utility functions.
+    global.Terra.viewports = getViewports;
+    global.Terra.hideInputCaret = hideInputCaret;
 
-      // validates provides access to the jest assertions to use in an it blocks.
-      validates: {
-        accessibility,
-        element,
-      },
-
-      // Provides access to Terra's list of supported testing viewports.
-      viewports: getViewports,
-
-      // Hides the blinking input caret that appears in inputs or editable text areas.
-      hideInputCaret,
-
-      axe: {
-        /**
-         * Global rule overrides.
-         * Rules modified here will be applied globally for all tests.
-         */
-        rules: {
-          /**
-           * This rule was introduced in axe-core v3.3 and causes failures in many Terra components.
-           * The solution to address this failure vary by component. It is being disabled until a solution is identified in the future.
-           *
-           * Reference: https://github.com/cerner/terra-framework/issues/991
-           */
-          'scrollable-region-focusable': { enabled: false },
-          /**
-           * The lowlight theme adheres to a non-default color contrast ratio and fails the default ratio check.
-           * The color-contrast ratio check is disabled for lowlight theme testing.
-           */
-          'color-contrast': { enabled: this.serviceOptions.theme !== 'clinical-lowlight-theme' },
-        },
-      },
-    };
+    // Setup and expose the validates utility functions.
+    global.Terra.validates = {};
+    // Bind the default selector to ensure it is always passes as the first parameter when calling validates element.
+    global.Terra.validates.element = element.bind(null, this.options.selector);
+    // Bind the global axe options to ensure they are always passes as the first parameter when calling validates accessibility.
+    global.Terra.validates.accessibility = accessibility.bind(null, this.axeOptions);
 
     // IE driver takes longer to be ready for browser interactions.
     if (capabilities.browserName === 'internet explorer') {
@@ -95,7 +95,7 @@ class TerraService {
     }
 
     // Set the viewport size before the spec begins.
-    setViewport(this.serviceOptions.formFactor);
+    setViewport(this.options.formFactor || 'huge');
   }
 
   afterCommand(commandName, _args, _result, error) {
