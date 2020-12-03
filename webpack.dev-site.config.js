b9fabc36f04707f0f0053acc7ba55bba051344ae
Matt Henkes <mjhenkes@gmail.com>
2020-12-03 17:02:20 -0600

    webpack 5

--- webpack.dev-site.config.js
+++ webpack.dev-site.config.js
@@ -3,43 +3,50 @@ const { merge } = require('webpack-merge');
 const path = require('path');
 const {
   TerraDevSite,
-  TerraDevSiteEntrypoints,
-  DirectorySwitcherPlugin,
-  LocalPackageAliasPlugin,
-} = require('terra-dev-site');
+} = require('@cerner/terra-dev-site');
 
 const WebpackConfigTerra = require('./packages/webpack-config-terra/webpack.config');
 /**
 * Generates the file representing app name configuration.
 */
-const devSiteConfig = (env = {}, argv = {}) => {
-  const production = argv.p;
-  const processPath = process.cwd();
-
-  return {
-    entry: TerraDevSiteEntrypoints,
-    plugins: [
-      new TerraDevSite({ env }),
-    ],
-    resolve: {
-      plugins: [
-        new DirectorySwitcherPlugin({
-          shouldSwitch: !production,
-          rootDirectories: [
-            processPath,
-            path.resolve(processPath, 'packages', '*'),
-          ],
-        }),
-        new LocalPackageAliasPlugin({
-          rootDirectories: [
-            processPath,
-            path.resolve(processPath, 'packages', '*'),
-          ],
-        }),
-      ],
-    },
-  };
-};
+const devSiteConfig = (env = {}) => ({
+  plugins: [
+    new TerraDevSite({
+      defaultLocale: env.defaultLocale,
+      primaryNavigationItems: [{
+        path: '/home',
+        label: 'Home',
+        contentExtension: 'home',
+        additionalContent: [
+          {
+            label: 'Home',
+            filePath: path.resolve(process.cwd(), 'README.md'),
+          },
+        ],
+      }, {
+        path: '/application',
+        label: 'Application',
+        contentExtension: 'app',
+      }, {
+        path: '/components',
+        label: 'Components',
+        contentExtension: 'doc',
+      }, {
+        path: '/dev_tools',
+        label: 'Developer Tools',
+        contentExtension: 'tool',
+      }, {
+        path: '/guides',
+        label: 'Guides',
+        contentExtension: 'guide',
+      }, {
+        path: '/tests',
+        label: 'Tests',
+        contentExtension: 'test',
+      }],
+    }),
+  ],
+});
 const webpackConfig = (env, argv) => (
   merge(WebpackConfigTerra(env, argv), devSiteConfig(env, argv))
 );
