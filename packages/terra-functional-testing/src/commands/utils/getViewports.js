9ebb43c2a9d7e06627363021b491d8946f58d1fa
Stephen Esser <stephen.esser@cerner.com>
2020-12-04 14:44:30 -0600

    Remove comment

--- packages/terra-functional-testing/src/commands/utils/getViewports.js
+++ packages/terra-functional-testing/src/commands/utils/getViewports.js
@@ -4,7 +4,6 @@ const ALL_VIEWPORTS = Object.keys(TERRA_VIEWPORTS);
 
 /**
  * Returns an array of viewport dimensions.
- * Each supported viewport has a width, height, and name.
  * @param {string|string[]} sizes - A list of Terra viewport sizes.
  * @return {array} - An array of viewports. Returns all viewports if sizes are not provided.
  */
