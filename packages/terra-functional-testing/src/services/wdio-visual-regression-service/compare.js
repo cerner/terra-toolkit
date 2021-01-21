d3a7dfb0c4f859f8315afe3f0df2674a5aca5f5b
Ben Cai <benbcai@gmail.com>
2021-01-21 17:56:28 -0600

    Remove commented out code.

--- packages/terra-functional-testing/src/services/wdio-visual-regression-service/compare.js
+++ packages/terra-functional-testing/src/services/wdio-visual-regression-service/compare.js
@@ -1,5 +1,3 @@
-// export { default as LocalCompare } from './methods/LocalCompare';
-// export { default as SaveScreenshot } from './methods/SaveScreenshot';
 const LocalCompare = require('./methods/LocalCompare');
 const SaveScreenshot = require('./methods/SaveScreenshot');
 
