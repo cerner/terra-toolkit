3bc0bb98fcb07645811908d59c76fd60561ac71a
Manuel,Ryan <Ryan.Manuel@Cerner.com>
2021-01-24 15:24:57 -0600

    Add a console.logged message that describes the upcoming release for use in notification channels

--- packages/terra-open-source-scripts/tests/jest/terra-cli/prepare-for-release/prepareForReleaseRepoHandler.test.js
+++ packages/terra-open-source-scripts/tests/jest/terra-cli/prepare-for-release/prepareForReleaseRepoHandler.test.js
@@ -3,17 +3,21 @@ jest.mock('npmlog');
 jest.mock('util');
 jest.mock('@npmcli/promise-spawn');
 jest.mock('../../../../src/terra-cli/prepare-for-release/updateChangelogForPackage');
+jest.mock('../../../../src/terra-cli/prepare-for-release/logReleasingPackages');
 
 const prompts = require('prompts');
+const path = require('path');
 const log = require('npmlog');
 const spawn = require('@npmcli/promise-spawn');
 const updateChangelogForPackage = require('../../../../src/terra-cli/prepare-for-release/updateChangelogForPackage');
+const logReleasingPackages = require('../../../../src/terra-cli/prepare-for-release/logReleasingPackages');
 
 const prepareForReleaseRepoHandler = require('../../../../src/terra-cli/prepare-for-release/prepareForReleaseRepoHandler');
 
 describe('prepareForReleaseRepoHandler', () => {
   it('prompts for a version, updates the version, and updates the change log of the current repo for a patch version', async () => {
     prompts.mockResolvedValueOnce({ versionType: 'patch' });
+    spawn.mockResolvedValueOnce({ stdout: 'v1.0.1' });
 
     await prepareForReleaseRepoHandler();
 
@@ -30,10 +34,12 @@ describe('prepareForReleaseRepoHandler', () => {
     expect(log.resume).toHaveBeenCalled();
     expect(spawn).toHaveBeenCalledWith('npm', ['--no-git-tag-version', 'version', 'patch'], { stdioString: true });
     expect(updateChangelogForPackage).toHaveBeenCalledWith(process.cwd());
+    expect(logReleasingPackages).toHaveBeenCalledWith([`${path.basename(process.cwd())}@1.0.1`]);
   });
 
   it('prompts for a version, updates the version, and updates the change log of the current repo for a minor version', async () => {
     prompts.mockResolvedValueOnce({ versionType: 'minor' });
+    spawn.mockResolvedValueOnce({ stdout: 'v1.1.0' });
 
     await prepareForReleaseRepoHandler();
 
@@ -50,10 +56,12 @@ describe('prepareForReleaseRepoHandler', () => {
     expect(log.resume).toHaveBeenCalled();
     expect(spawn).toHaveBeenCalledWith('npm', ['--no-git-tag-version', 'version', 'minor'], { stdioString: true });
     expect(updateChangelogForPackage).toHaveBeenCalledWith(process.cwd());
+    expect(logReleasingPackages).toHaveBeenCalledWith([`${path.basename(process.cwd())}@1.1.0`]);
   });
 
   it('prompts for a version, updates the version, and updates the change log of the current repo for a major version', async () => {
     prompts.mockResolvedValueOnce({ versionType: 'major' });
+    spawn.mockResolvedValueOnce({ stdout: 'v2.0.0' });
 
     await prepareForReleaseRepoHandler();
 
@@ -70,5 +78,6 @@ describe('prepareForReleaseRepoHandler', () => {
     expect(log.resume).toHaveBeenCalled();
     expect(spawn).toHaveBeenCalledWith('npm', ['--no-git-tag-version', 'version', 'major'], { stdioString: true });
     expect(updateChangelogForPackage).toHaveBeenCalledWith(process.cwd());
+    expect(logReleasingPackages).toHaveBeenCalledWith([`${path.basename(process.cwd())}@2.0.0`]);
   });
 });
