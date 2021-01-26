b0d14f65502f74b648ca65bcc5da48eba897b481
Derek Yu <yuderekyu@gmail.com>
2021-01-26 14:34:02 -0600

    create a reduced test cause using vanilla selenium.

--- packages/terra-functional-testing/tests/wdio/test-spec.js
+++ packages/terra-functional-testing/tests/wdio/test-spec.js
@@ -1,8 +1,24 @@
-describe('Reduce Test Case: Terra + Test service -> 2 afterCommands-> 2 browser.executes', () => {
-  it('executes both afterCommands with no issues', () => {
-    browser.url('https://engineering.cerner.com/terra-application/tests/terra-application/application-base/private/test-overrides-test/');
-    browser.refresh();
-    // Terra.saveScreenshot();
-    browser.saveScreenshot('ie-verification.png');
-  });
-});
+const { Builder, Capabilities } = require("selenium-webdriver");
+const ie = require('selenium-webdriver/ie');
+const { ie : ieOptions } = require('../../src/config/capabilities');
+
+(async function reducedTestCase() {
+  const driver = new Builder()
+    .usingServer("http://10.188.194.84:4444/wd/hub")
+    .withCapabilities(Capabilities.ie(ieOptions))
+    .build();
+  try {
+    await driver.get('https://engineering.cerner.com/terra-application/tests/terra-application/application-base/private/test-overrides-test/');
+    await driver.executeScript(() => testFn(1));
+    await driver.executeScript(() => testFn(2));
+    await driver.navigate().refresh();
+    await driver.executeScript(() => testFn(3));
+    await driver.executeScript(() => testFn(4));
+  } finally {
+    await driver.quit();
+  }
+})();
+
+function testFn (number) {
+  console.log(`[${number}]execute testFn`);
+}
