88f0eac64b0f4c17fc2d2da031653fd0be66b18c
Stephen Esser <stephen.esser@cerner.com>
2021-01-26 09:54:48 -0600

    Remove dynamic test directory logic

--- packages/terra-functional-testing/src/reporters/spec-reporter/get-output-dir.js
+++ packages/terra-functional-testing/src/reporters/spec-reporter/get-output-dir.js
@@ -1,18 +1,9 @@
-const fs = require('fs');
 const path = require('path');
 
 /**
- * Determines the output directory for reporter results.
+ * Returns the default output directory for reporter results.
  * @returns {string} - An output directory.
  */
-const getOutputDir = () => {
-  const cwd = process.cwd();
-
-  if (fs.existsSync(path.join(cwd, 'test'))) {
-    return path.resolve(cwd, 'test', 'wdio', 'reports');
-  }
-
-  return path.resolve(cwd, 'tests', 'wdio', 'reports');
-};
+const getOutputDir = () => path.resolve(process.cwd(), 'tests', 'wdio', 'reports');
 
 module.exports = getOutputDir;
--- packages/terra-functional-testing/tests/jest/reporters/spec-reporter/get-output-dir.test.js
+++ packages/terra-functional-testing/tests/jest/reporters/spec-reporter/get-output-dir.test.js
@@ -1,24 +1,7 @@
-const fs = require('fs');
 const getOutputDir = require('../../../../src/reporters/spec-reporter/get-output-dir');
 
 describe('getOutputDir', () => {
-  afterEach(() => {
-    // Restore all fs mocks.
-    jest.restoreAllMocks();
-  });
-
-  it('should return test as the output directory if it exists', () => {
-    jest.spyOn(fs, 'existsSync').mockImplementationOnce(() => true);
-
-    const outputDir = getOutputDir();
-
-    expect(fs.existsSync).toHaveBeenCalledWith(expect.stringContaining('/test'));
-    expect(outputDir).toEqual(expect.stringContaining('/test/wdio/reports'));
-  });
-
-  it('should return tests as the output directory if test does not exist', () => {
-    jest.spyOn(fs, 'existsSync').mockImplementationOnce(() => false);
-
+  it('should return the expected output directory', () => {
     const outputDir = getOutputDir();
 
     expect(outputDir).toEqual(expect.stringContaining('/tests/wdio/reports'));
