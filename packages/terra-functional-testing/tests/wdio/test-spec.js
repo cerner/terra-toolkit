bd31b7571d854e77271eaf39aaff7dcb3a5378d2
Derek Yu <yuderekyu@gmail.com>
2021-01-26 17:03:53 -0600

    update test case. it works..

--- packages/terra-functional-testing/tests/wdio/test-spec.js
+++ packages/terra-functional-testing/tests/wdio/test-spec.js
@@ -2,6 +2,14 @@ const { Builder, Capabilities } = require("selenium-webdriver");
 const ie = require('selenium-webdriver/ie');
 const { ie : ieOptions } = require('../../src/config/capabilities');
 
+function dispatchEvent(eventName, eventMetaData) {
+  /* If IE support is removed, convert below to use event constructors. */
+  const event = document.createEvent('Event');
+  event.initEvent(eventName, true, true);
+  event.metaData = eventMetaData;
+  window.dispatchEvent(event);
+}
+
 (async function reducedTestCase() {
   const driver = new Builder()
     .usingServer("http://10.188.194.84:4444/wd/hub")
@@ -9,16 +17,16 @@ const { ie : ieOptions } = require('../../src/config/capabilities');
     .build();
   try {
     await driver.get('https://engineering.cerner.com/terra-application/tests/terra-application/application-base/private/test-overrides-test/');
-    await driver.executeScript(() => testFn(1));
-    await driver.executeScript(() => testFn(2));
+    // await driver.executeScript(function() { dispatchEvent('applicationBase.testOverride', { locale: 'es' }); });
+    // await driver.executeScript(function() { dispatchEvent('applicationBase.testOverride', { locale: 'fr' }); });
+    await driver.executeScript("window.open()");
+    await driver.executeScript("console.log('test')");
     await driver.navigate().refresh();
-    await driver.executeScript(() => testFn(3));
-    await driver.executeScript(() => testFn(4));
+    // await driver.executeScript(function() { dispatchEvent('applicationBase.testOverride', { locale: 'de' }); });
+    // await driver.executeScript(function() { dispatchEvent('applicationBase.testOverride', { locale: 'pt' }); });
+    await driver.executeScript("console.log('test')");
+    await driver.executeScript("console.log('test')");
   } finally {
     await driver.quit();
   }
 })();
-
-function testFn (number) {
-  console.log(`[${number}]execute testFn`);
-}
