59d2ff088ce2769777d030b90b0c473e800fb35d
Jaime Mackey <jaime.mackey@cerner.com>
2021-01-27 17:26:41 -0600

    .

--- packages/terra-aggregate-translations/bin/aggregate-translations-cli.js
+++ packages/terra-aggregate-translations/bin/aggregate-translations-cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const commander = require('commander');
-const supportedLocales = require('../config/i18nSupportedLocales');
+const supportedLocales = require('../lib/config/i18nSupportedLocales');
 
 const aggregateTranslations = require('../lib/aggregate-translations');
 
