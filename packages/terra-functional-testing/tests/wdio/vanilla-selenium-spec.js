20cd0f9e2fbd02984bf9e963fe3abb0e9f592370
Derek Yu <yuderekyu@gmail.com>
2021-01-27 15:34:09 -0600

    execute locale change.

--- packages/terra-functional-testing/tests/wdio/vanilla-selenium-spec.js
+++ packages/terra-functional-testing/tests/wdio/vanilla-selenium-spec.js
@@ -1,6 +1,4 @@
 const { Builder, Capabilities } = require("selenium-webdriver");
-const ie = require('selenium-webdriver/ie');
-const { ie : ieOptions } = require('../../src/config/capabilities');
 
 function dispatchEvent(eventName, eventMetaData) {
   /* If IE support is removed, convert below to use event constructors. */
@@ -13,19 +11,36 @@ function dispatchEvent(eventName, eventMetaData) {
 (async function reducedTestCase() {
   const driver = new Builder()
     .usingServer("http://10.188.198.242:4444/wd/hub")
-    .withCapabilities(Capabilities.ie(ieOptions))
+    .withCapabilities(Capabilities.ie())
     .build();
   try {
     await driver.get('https://engineering.cerner.com/terra-application/tests/terra-application/application-base/private/test-overrides-test/');
-    await driver.executeScript(function(event) {
-      dispatchEvent(event.name, event.metaData);
-    }, { name: 'applicationBase.testOverride', metaData: { locale: 'en-US' }});
-    await driver.executeScript(function() { console.log('test'); });
+    await driver.executeScript(function(name, metaData) {
+      const event = document.createEvent('Event');
+      event.initEvent(name, true, true);
+      event.metaData = metaData;
+      window.dispatchEvent(event);
+    }, 'applicationBase.testOverride', { locale: 'en-US' });
+    await driver.executeScript(function(name, metaData) {
+      const event = document.createEvent('Event');
+      event.initEvent(name, true, true);
+      event.metaData = metaData;
+      window.dispatchEvent(event);
+    }, 'applicationBase.testOverride', { locale: 'de' });
     await driver.navigate().refresh();
-    await driver.executeScript(function(event) {
-      dispatchEvent(event.name, event.metaData);
-    }, { name: 'applicationBase.testOverride', metaData: { locale: 'pt' }});
-    await driver.executeScript(function() { console.log('test'); });
+    await driver.executeScript(function(name, metaData) {
+      const event = document.createEvent('Event');
+      event.initEvent(name, true, true);
+      event.metaData = metaData;
+      window.dispatchEvent(event);
+    }, 'applicationBase.testOverride', { locale: 'fr' });
+    await driver.takeScreenshot();
+    // await driver.executeScript(function(event) {
+    //   dispatchEvent(event.name, event.metaData);
+    // }, { name: 'applicationBase.testOverride', metaData: { locale: 'pt' }});
+    // await driver.executeScript(function() { console.log('test'); });
+  } catch(err) {
+    throw new Error(`error: ${err}`);
   } finally {
     await driver.quit();
   }
