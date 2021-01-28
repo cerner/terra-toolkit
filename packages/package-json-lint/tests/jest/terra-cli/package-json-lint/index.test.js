dde9f692f250c1f76c4c13cde4d0448b399ce6a6
Manuel,Ryan <Ryan.Manuel@Cerner.com>
2021-01-27 20:35:51 -0600

    Create package json lint command

---
+++ packages/package-json-lint/tests/jest/terra-cli/package-json-lint/index.test.js
@@ -0,0 +1,32 @@
+jest.mock('../../../../src/lint');
+
+const yargs = require('yargs');
+const lint = require('../../../../src/lint');
+const PrepareForReleaseCommand = require('../../../../src/terra-cli/package-json-lint');
+
+describe('package-json-lint', () => {
+  it('declares the package-json-lint terra-cli command with proper top level help', async () => {
+    const parser = yargs.command(PrepareForReleaseCommand).scriptName('terra').help();
+    const helpOutput = await new Promise((resolve) => {
+      parser.parse('--help', (err, argv, output) => {
+        resolve(output);
+      });
+    });
+    expect(helpOutput).toMatchSnapshot();
+  });
+
+  it('declares the package-json-lint terra-cli command with proper command level help', async () => {
+    const parser = yargs.command(PrepareForReleaseCommand).scriptName('terra').help();
+    const helpOutput = await new Promise((resolve) => {
+      parser.parse('prepare-for-release --help', (err, argv, output) => {
+        resolve(output);
+      });
+    });
+    expect(helpOutput).toMatchSnapshot();
+  });
+
+  it('uses the appropriate handler when calling package-json-lint', async () => {
+    await PrepareForReleaseCommand.handler();
+    expect(lint).toHaveBeenCalled();
+  });
+});
