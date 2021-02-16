85822ef1811fc3061a8c3f201e8e0f90e505ce83
Stephen Esser <Stephen.Esser@gmail.com>
2021-02-16 16:33:12 -0600

    [terra-functional-testing] Update documentation and remove test* spec regex (#577)

--- packages/terra-functional-testing/src/config/wdio.conf.js
+++ packages/terra-functional-testing/src/config/wdio.conf.js
@@ -46,8 +46,8 @@ exports.config = {
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
   specs: [
-    './test*/wdio/**/*-spec.js',
-    './packages/*/test*/wdio/**/*-spec.js',
+    './tests/wdio/**/*-spec.js',
+    './packages/*/tests/wdio/**/*-spec.js',
   ],
   //
   // ============
