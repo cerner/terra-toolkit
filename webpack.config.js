a4405fd8085950a2f8c1a9f41af54ca6bc808d43
Matt Henkes <mjhenkes@gmail.com>
2021-02-19 09:21:40 -0600

    Re-enabling terra-dev-site by pulling in the experimental terra-dev-site and terra-application branches.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env = {}) => {
