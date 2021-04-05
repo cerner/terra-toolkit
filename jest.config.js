5daa88dd3d3370b260b546f8b253216d55137852
Manuel,Ryan <Ryan.Manuel@Cerner.com>
2021-04-05 13:34:16 -0500

    Add back in modulePathIgnorePatterns to avoid jest-haste-map warnings

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ const jestConfig = require('./packages/jest-config-terra');
 
 module.exports = {
   ...jestConfig,
+  modulePathIgnorePatterns: [
+    'packages/terra-cli/tests/jest/fixtures',
+    'packages/duplicate-package-checker-webpack-plugin/tests/jest',
+  ],
   testEnvironment: './packages/jest-config-terra/lib/JestEnvironmentJsdomTerra.js',
   setupFiles: [
     './jest.enzymeSetup.js',
