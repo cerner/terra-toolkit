ffa84483733ecf2a26fdc6dbdfe0df8184bbd2a2
pranav agarwal <agarwal.pranav300@gmail.com>
2021-04-28 15:52:53 +0530

    minor changes

--- packages/terra-functional-testing/src/services/wdio-visual-regression-service/methods/BaseCompare.js
+++ packages/terra-functional-testing/src/services/wdio-visual-regression-service/methods/BaseCompare.js
@@ -20,7 +20,7 @@ class BaseCompare {
     } = options;
 
     // screenshot naming config
-    console.log("cdvevrcver", baseScreenshotDir)
+    console.log("Base Screenshot Dir", baseScreenshotDir)
     this.baseScreenshotDir = baseScreenshotDir;
     this.locale = locale || 'en';
     this.theme = theme || 'terra-default-theme';
