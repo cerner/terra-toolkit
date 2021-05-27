bfd874b32bbf7150606efcbdaa08a9b2f601bf64
Sireesha Nekkanti <C_Nekkanti.Sireesha@Cerner.com>
2021-05-27 12:02:32 -0600

    file output reporter added

---
+++ packages/terra-functional-testing/src/reporters/fileOutput-reporter/index.js
@@ -0,0 +1,211 @@
+const fs = require('fs-extra');
+const path = require('path');
+const stripAnsi = require('strip-ansi');
+const SpecReporter = require('@wdio/spec-reporter').default;
+const endOfLine = require('os').EOL;
+const { Logger } = require('@cerner/terra-cli');
+
+const LOG_CONTEXT = '[Terra-Toolkit:fileOutput-reporter]';
+
+class FileOutputReporter extends SpecReporter {
+  constructor() {
+    super();
+    this.runners = [];
+    this.resultJsonObject = {
+      startDate: '',
+      type: 'wdio',
+      locale: '',
+      formFactor: '',
+      theme: '',
+      output: {},
+      endDate: '',
+    };
+    this.fileName = '';
+    this.moduleName = process.cwd().split(path.sep).pop();
+
+    this.setResultsDir = this.setResultsDir.bind(this);
+    this.hasResultsDir = this.hasResultsDir.bind(this);
+    this.setTestModule = this.setTestModule.bind(this);
+    this.printReport = this.printReport.bind(this);
+    this.getMessage = this.getMessage.bind(this);
+
+    this.setResultsDir();
+    this.hasResultsDir();
+  }
+
+  /**
+   * Sets results directory for the test run. Uses the wdio reporterOptions.outputDir if set, otherwise
+   * it outputs to tests?/wdio/reports.
+   * @return null;
+   */
+  setResultsDir() {
+    let testDir = 'tests';
+    if (fs.existsSync(path.join(process.cwd(), 'test'))) {
+      testDir = 'test';
+    }
+    this.resultsDir = path.join(process.cwd(), testDir, 'wdio', 'reports');
+  }
+
+  /**
+   * Check and create reports dir if doesn't exist
+   * @return null
+   */
+  hasResultsDir() {
+    if (!fs.existsSync(this.resultsDir)) {
+      fs.mkdirSync(this.resultsDir, { recursive: true }, (err) => {
+        if (err) {
+          Logger.error(err.message, { context: LOG_CONTEXT });
+        }
+      });
+    }
+  }
+
+  /**
+   * Formatting the filename based on locale, theme, and formFactor
+   * @return null
+   */
+  fileNameCheck({ formFactor, locale, theme }, browserName, cid) {
+    const fileNameConf = ['fileOutput'];
+    if (locale) {
+      fileNameConf.push(locale);
+      this.resultJsonObject.locale = locale;
+    }
+
+    if (theme) {
+      fileNameConf.push(theme);
+      this.resultJsonObject.theme = theme;
+    }
+
+    if (formFactor) {
+      fileNameConf.push(formFactor);
+      this.resultJsonObject.formFactor = formFactor;
+    }
+
+    if (browserName) {
+      fileNameConf.push(browserName);
+    }
+
+    if (cid) {
+      fileNameConf.push(cid);
+    }
+
+    this.fileName = fileNameConf.join('-');
+  }
+
+  /**
+   * Set the package name to moduleName property if specsValue contains /package string
+   * @param {string} specsValue - File path of current spec file from runners
+   * @return null
+   */
+  setTestModule(specsValue) {
+    const index = specsValue.lastIndexOf(`packages${path.sep}`);
+    if (index > -1) {
+      const testFilePath = specsValue.substring(index).split(path.sep);
+      const moduleName = testFilePath && testFilePath[1]
+        ? testFilePath[1]
+        : process.cwd().split(path.sep).pop();
+      if (moduleName && moduleName !== this.moduleName) {
+        this.moduleName = moduleName;
+      }
+    }
+  }
+
+  onRunnerEnd(runner) {
+    this.runners.push(runner);
+    this.printReport(runner);
+  }
+
+  getMessage(runner) {
+    const results = this.getResultDisplay();
+    if (results.length === 0) {
+      return null;
+    }
+    const testLinks = runner.isMultiremote
+      ? Object.entries(runner.capabilities).map(
+        ([instanceName, capabilities]) => this.getTestLink({
+          config: { ...runner.config, ...{ capabilities } },
+          sessionId: capabilities.sessionId,
+          isMultiremote: runner.isMultiremote,
+          instanceName,
+        }),
+      )
+      : this.getTestLink(runner);
+    const output = [
+      ...this.getHeaderDisplay(runner),
+      '',
+      ...results,
+      ...this.getFailureDisplay(),
+      ...(testLinks.length ? ['', ...testLinks] : []),
+    ];
+    const preface = `[${this.getEnviromentCombo(
+      runner.capabilities,
+      false,
+      runner.isMultiremote,
+    ).trim()} #${runner.cid}]`;
+    const divider = '------------------------------------------------------------------';
+    const prefacedOutput = output.map((value) => (value ? `${preface} ${value}` : preface));
+    return `${divider}\n${prefacedOutput}`;
+  }
+
+  printReport() {
+    const { runners } = this;
+    if (runners && runners.length) {
+      runners.forEach((runner, index) => {
+        // determine correct file name given configuration for run
+        if (index === 0) {
+          const { cid, capabilities, config } = runner;
+          const { browserName } = capabilities;
+          this.fileNameCheck(config.launcherOptions, browserName, cid);
+        }
+
+        this.setTestModule(runner.specs[0]);
+
+        if (!this.resultJsonObject.output[this.moduleName]) {
+          this.resultJsonObject.output[this.moduleName] = [];
+        }
+        const readableMessage = this.getMessage(runner);
+        const readableArrayMessage = readableMessage.toString().split(',');
+        const messages = readableArrayMessage.map((message) => stripAnsi(`${message}${endOfLine}`));
+        this.resultJsonObject.output[this.moduleName].push(messages.join(''));
+        this.resultJsonObject.startDate = new Date(
+          runner.start,
+        ).toLocaleString();
+        this.resultJsonObject.endDate = new Date(runner.end).toLocaleString();
+      });
+    }
+    const {
+      endDate, startDate, locale, formFactor, theme, output,
+    } = this.resultJsonObject;
+
+    const moduleKeys = Object.keys(output) || [];
+    if (output && moduleKeys.length) {
+      moduleKeys.forEach((key) => {
+        const fileData = {
+          startDate,
+          locale,
+          theme,
+          formFactor,
+          output: output[key],
+          endDate,
+        };
+
+        const filePathLocation = path.join(
+          this.resultsDir,
+          `${this.fileName}-${key}.json`,
+        );
+        fs.writeFileSync(
+          filePathLocation,
+          `${JSON.stringify(fileData, null, 2)}`,
+          { flag: 'w+' },
+          (err) => {
+            if (err) {
+              Logger.error(err.message, { context: LOG_CONTEXT });
+            }
+          },
+        );
+      });
+    }
+  }
+}
+
+module.exports = FileOutputReporter;
---
+++ packages/terra-functional-testing/tests/jest/reporters/fileOutput-reporter/index.test.js
@@ -0,0 +1,215 @@
+import fs from 'fs';
+import path from 'path';
+import FileOutputReporter from '../../../../src/reporters/fileOutput-reporter';
+
+jest.mock('fs');
+
+describe('FileOutputReporter', () => {
+  const originalProcessCwd = process.cwd;
+  beforeAll(() => {
+    process.cwd = jest.fn().mockImplementation(() => './terra-toolkit');
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(() => {
+    process.cwd = originalProcessCwd;
+  });
+
+  describe('initialization', () => {
+    it('defines resultJsonObject', () => {
+      const reporter = new FileOutputReporter({}, {});
+      expect(reporter.resultJsonObject).toHaveProperty('output');
+      expect(reporter.resultJsonObject).toHaveProperty('startDate');
+      expect(reporter.resultJsonObject).toHaveProperty('endDate');
+      expect(reporter.resultJsonObject).toHaveProperty('type');
+      expect(typeof reporter.resultJsonObject.output).toEqual('object');
+    });
+
+    it('defines fileName', () => {
+      const reporter = new FileOutputReporter({}, {});
+      expect(reporter.fileName).toBe('');
+    });
+
+    it('defines moduleName', () => {
+      const reporter = new FileOutputReporter({}, {});
+      expect(reporter.moduleName).toBe('terra-toolkit');
+    });
+
+    describe('determines results dir', () => {
+      it('set outputDir ', () => {
+        fs.existsSync.mockReturnValue(true);
+        let reporter = new FileOutputReporter({}, {});
+        expect(reporter.resultsDir).toEqual(expect.stringContaining('test/wdio/reports'));
+
+        fs.existsSync.mockReturnValue(false);
+        reporter = new FileOutputReporter({}, {});
+        expect(reporter.resultsDir).toEqual(expect.stringContaining('tests/wdio/reports'));
+      });
+    });
+
+    describe('ensures results dir exists', () => {
+      it('when dir exists', () => {
+        fs.existsSync.mockReturnValue(true);
+        const reporter = new FileOutputReporter({}, {});
+        reporter.hasResultsDir();
+        expect(fs.mkdirSync).not.toHaveBeenCalled();
+      });
+
+      it('when dir does not exists', () => {
+        fs.existsSync.mockReturnValue(false);
+        const reporter = new FileOutputReporter({}, {});
+        reporter.hasResultsDir();
+        expect(fs.mkdirSync).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('fileNameCheck', () => {
+    it('sets default file name', () => {
+      const reporter = new FileOutputReporter({}, {});
+      reporter.fileNameCheck({ }, undefined, '0-1');
+      expect(reporter.fileName).toEqual('fileOutput-0-1');
+      expect(reporter.resultJsonObject).toHaveProperty('locale', '');
+      expect(reporter.resultJsonObject).toHaveProperty('theme', '');
+      expect(reporter.resultJsonObject).toHaveProperty('formFactor', '');
+    });
+
+    it('sets file name with locale', () => {
+      const reporter = new FileOutputReporter({}, {});
+      reporter.fileNameCheck({ locale: 'en' }, undefined, '0-1');
+      expect(reporter.fileName).toEqual('fileOutput-en-0-1');
+      expect(reporter.resultJsonObject).toHaveProperty('locale', 'en');
+      expect(reporter.resultJsonObject).toHaveProperty('theme', '');
+      expect(reporter.resultJsonObject).toHaveProperty('formFactor', '');
+    });
+
+    it('sets file name with locale and theme', () => {
+      const reporter = new FileOutputReporter({}, {});
+      reporter.fileNameCheck({ locale: 'en', theme: 'default' }, undefined, '0-1');
+      expect(reporter.fileName).toEqual('fileOutput-en-default-0-1');
+      expect(reporter.resultJsonObject).toHaveProperty('locale', 'en');
+      expect(reporter.resultJsonObject).toHaveProperty('theme', 'default');
+      expect(reporter.resultJsonObject).toHaveProperty('formFactor', '');
+    });
+
+    it('sets file name with locale, theme and formFactor', () => {
+      const reporter = new FileOutputReporter({}, {});
+      reporter.fileNameCheck({ locale: 'en', theme: 'default', formFactor: 'tiny' }, undefined, '0-1');
+      expect(reporter.fileName).toEqual('fileOutput-en-default-tiny-0-1');
+      expect(reporter.resultJsonObject).toHaveProperty('locale', 'en');
+      expect(reporter.resultJsonObject).toHaveProperty('theme', 'default');
+      expect(reporter.resultJsonObject).toHaveProperty('formFactor', 'tiny');
+    });
+
+    it('sets file name with locale, theme, formFactor, browser and cid', () => {
+      const reporter = new FileOutputReporter({}, {});
+      reporter.fileNameCheck({ locale: 'en', theme: 'default', formFactor: 'tiny' }, 'chrome', '0-1');
+      expect(reporter.fileName).toEqual('fileOutput-en-default-tiny-chrome-0-1');
+      expect(reporter.resultJsonObject).toHaveProperty('locale', 'en');
+      expect(reporter.resultJsonObject).toHaveProperty('theme', 'default');
+      expect(reporter.resultJsonObject).toHaveProperty('formFactor', 'tiny');
+    });
+  });
+
+  describe('setTestModule', () => {
+    it('updates moduleName if mono-repo test file', () => {
+      const reporter = new FileOutputReporter({}, {});
+      expect(reporter.moduleName).toEqual('terra-toolkit');
+      reporter.setTestModule('terra-toolkit/packages/my-package/tests/wdio/test-spec.js');
+      expect(reporter.moduleName).toEqual('my-package');
+    });
+
+    it('updates moduleName if mono-repo test file if windows path', () => {
+      const reporter = new FileOutputReporter({}, {});
+      const separator = path.sep;
+      path.sep = '\\';
+
+      expect(reporter.moduleName).toEqual('terra-toolkit');
+      reporter.setTestModule('C:\\project\\packages\\my-package\\tests\\wdio\\test-spec.js');
+      expect(reporter.moduleName).toEqual('my-package');
+
+      path.sep = separator;
+    });
+
+    it('does not updates moduleName if non mono-repo test file', () => {
+      const reporter = new FileOutputReporter({}, {});
+      expect(reporter.moduleName).toEqual('terra-toolkit');
+      reporter.setTestModule('terra-functional-testing/tests/wdio/test-spec.js');
+      expect(reporter.moduleName).toEqual('terra-toolkit');
+    });
+  });
+
+  describe('printReport', () => {
+    const runner = {
+      cid: '0-0',
+      specs: ['/packages/terra-functional-testing/tests/wdio/hideInputCaret-spec.js'],
+      capabilities: {
+        browserName: 'firefox',
+      },
+      config: {
+        launcherOptions: {
+          locale: 'en',
+          theme: 'terra-default-theme',
+          formFactor: 'small',
+        },
+      },
+    };
+    describe('when no specs run - does not write results file', () => {
+      fs.writeFileSync.mockImplementation(() => {});
+      const reporter = new FileOutputReporter({}, {});
+      reporter.runners = {};
+      expect(fs.writeFileSync).not.toHaveBeenCalled();
+    });
+
+    it('when one spec runs - write results file', () => {
+      fs.writeFileSync.mockImplementation(() => {});
+      const reporter = new FileOutputReporter({}, {});
+      reporter.runners = [runner];
+      reporter.getSuiteResult = jest.fn().mockReturnValue('wdio test results');
+      const getMessage = jest.fn();
+      reporter.getMessage = getMessage;
+      reporter.getMessage.mockReturnValue('[chrome 69.0.3497.100 Linux #0-2] Spec: /Users/sn081183/Desktop/terra/terra-toolkit/packages/terra-functional-testing/tests/wdio/terra-validates-spec.js,[chrome 69.0.3497.100 Linux #0-2] Running: chrome (v69.0.3497.100) on Linux,[chrome 69.0.3497.100 Linux #0-2] Session ID: 8bc8d5b1a51f746454ff9714b57c9fd8,[chrome 69.0.3497.100 Linux #0-2],[chrome 69.0.3497.100 Linux #0-2] [small],[chrome 69.0.3497.100 Linux #0-2]  ');
+      reporter.printReport();
+      expect(reporter.resultJsonObject).toHaveProperty('output');
+      expect(reporter.resultJsonObject.output).toHaveProperty('terra-functional-testing');
+      expect(reporter.resultJsonObject.output['terra-functional-testing']).toHaveLength(1);
+      expect(fs.writeFileSync).toHaveBeenCalled();
+    });
+
+    it('when many specs run - write results file', () => {
+      fs.writeFileSync.mockImplementation(() => {});
+      const reporter = new FileOutputReporter({}, {});
+      reporter.runners = [runner, { ...runner, cid: '0-1' }];
+      reporter.getSuiteResult = jest.fn().mockReturnValue('wdio test results\n');
+      const getMessage = jest.fn();
+      reporter.getMessage = getMessage;
+      reporter.getMessage.mockReturnValue('[chrome 69.0.3497.100 Linux #0-2] Spec: /Users/sn081183/Desktop/terra/terra-toolkit/packages/terra-functional-testing/tests/wdio/terra-validates-spec.js,[chrome 69.0.3497.100 Linux #0-2] Running: chrome (v69.0.3497.100) on Linux,[chrome 69.0.3497.100 Linux #0-2] Session ID: 8bc8d5b1a51f746454ff9714b57c9fd8,[chrome 69.0.3497.100 Linux #0-2],[chrome 69.0.3497.100 Linux #0-2] [small],[chrome 69.0.3497.100 Linux #0-2]  ');
+      reporter.printReport();
+      expect(reporter.resultJsonObject).toHaveProperty('output');
+      expect(reporter.resultJsonObject.output).toHaveProperty('terra-functional-testing');
+
+      expect(reporter.resultJsonObject.output['terra-functional-testing']).toHaveLength(2);
+      expect(fs.writeFileSync).toHaveBeenCalled();
+    });
+  });
+
+  describe('printSuitesSummary', () => {
+    const stats = {
+      start: 'Tue Apr 28 2020 12:14:56',
+      end: 'Tue Apr 28 2020 12:14:59',
+    };
+
+    it('adds start and end dates to resultJsonObject', () => {
+      const reporter = new FileOutputReporter({}, {});
+      reporter.baseReporter = { stats };
+      reporter.printReport = jest.fn();
+      reporter.printReport();
+      expect(reporter.resultJsonObject).toHaveProperty('endDate');
+      expect(reporter.resultJsonObject).toHaveProperty('startDate');
+      expect(reporter.printReport).toHaveBeenCalled();
+    });
+  });
+});
