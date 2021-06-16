9dec7d6db736eeead4a7a3625f5f69d0f5e97530
Ben Cai <benbcai@gmail.com>
2021-06-16 11:21:27 -0500

    return viewport size

--- packages/terra-functional-testing/src/commands/utils/getViewportSize.js
+++ packages/terra-functional-testing/src/commands/utils/getViewportSize.js
@@ -3,11 +3,13 @@
  * @return {Object} - viewport width and height of the current browser window.
  */
 const getViewportSize = () => {
-  global.browser.execute(() => {
+  const viewportSize = global.browser.execute(() => {
     const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     return { width, height };
   });
+
+  return viewportSize;
 };
 
 module.exports = getViewportSize;
