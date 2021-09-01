cc6d1e0cc26383d90b787febbf2eeec56d41464a
pranav agarwal <agarwal.pranav300@gmail.com>
2021-09-01 23:47:27 +0530

    updated projectType to devModule for toolkit

--- packages/package-json-lint-config-terra/package-json-lint.config.js
+++ packages/package-json-lint-config-terra/package-json-lint.config.js
@@ -2,6 +2,6 @@ module.exports = {
   rules: {
     'require-no-terra-base-peer-dependency-versions': 'warn',
     'require-theme-context-versions': 'warn',
-    'require-no-hard-coded-dependency-versions': 'warn',
+    'require-no-hard-coded-dependency-versions': 'error',
   },
 };
