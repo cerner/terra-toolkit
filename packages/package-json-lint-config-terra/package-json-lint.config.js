ad341948e852ed58678eebf1652724dc3e3fa845
pranav agarwal <agarwal.pranav300@gmail.com>
2021-09-02 00:50:33 +0530

    minor change

--- packages/package-json-lint-config-terra/package-json-lint.config.js
+++ packages/package-json-lint-config-terra/package-json-lint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   rules: {
+    'require-no-hard-coded-dependency-versions': 'error',
     'require-no-terra-base-peer-dependency-versions': 'warn',
     'require-theme-context-versions': 'warn',
-    'require-no-hard-coded-dependency-versions': 'error',
   },
 };
