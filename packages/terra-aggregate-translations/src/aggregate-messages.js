4aff8266f0b81e7f88dedfe4c98b039deaeb8b8d
Ryan Manuel <rfmanuel@gmail.com>
2021-09-14 15:52:29 -0500

    Update aggregate-messages.js

--- packages/terra-aggregate-translations/src/aggregate-messages.js
+++ packages/terra-aggregate-translations/src/aggregate-messages.js
@@ -13,9 +13,13 @@ const aggregateTranslationMessages = (translationDirectories, locales, fileSyste
     try {
       Object.assign(translations[language], JSON.parse(fileSystem.readFileSync(translationFile, 'utf8')));
     } catch (e) {
+      // Only warn if the JSON is invalid or if the base language is missing
       if (e instanceof SyntaxError) {
         /* eslint-disable-next-line no-console */
         console.warn(chalk.yellow(`There was an error reading your translations file ${translationFile}.\n Exception Message: ${e.message} \n`));
+      } else if (language.split('-').length === 1) {
+        /* eslint-disable-next-line no-console */
+        console.warn(chalk.yellow(`The base translation file for ${language} is missing at ${translationFile}.\n`));
       }
     }
   }));
