da6a5b835fce4789f8abc824358c016f4b6cf54c
pranav agarwal <agarwal.pranav300@gmail.com>
2021-09-27 23:02:21 +0530

    Added devDependencySet

--- packages/package-json-lint/src/rules/require-no-unnecessary-dependencies.js
+++ packages/package-json-lint/src/rules/require-no-unnecessary-dependencies.js
@@ -1,78 +1,76 @@
-const devPeerDependencySet = [
+const devDependencySet = [
   '@babel/cli',
   '@babel/core',
   '@babel/helpers',
   '@babel/plugin-proposal-object-rest-spread',
   '@babel/plugin-syntax-async-generators',
-  '@babel/plugin-transform-object-assign',
   '@babel/plugin-transform-regenerator',
   '@babel/plugin-transform-runtime',
   '@babel/preset-env',
   '@babel/preset-react',
   '@babel/register',
-  '@cerner/browserslist-config-terra',
-  '@cerner/eslint-config-terra',
-  '@cerner/jest-config-terra',
-  '@cerner/package-json-lint',
-  '@cerner/stylelint-config-terra',
-  '@cerner/terra-aggregate-translations',
   '@cerner/terra-application',
-  '@cerner/terra-cli',
   '@cerner/terra-dev-site',
-  '@cerner/terra-functional-testing',
-  '@cerner/terra-open-source-scripts',
-  '@cerner/webpack-config-terra',
   '@octokit/core',
   'bufferutil',
-  'chai',
-  'check-installed-dependencies',
   'commander',
-  'core-js',
-  'csvtojson',
   'danger',
   'enzyme',
   'enzyme-adapter-react-16',
   'enzyme-to-json',
   'eslint',
-  'gh-pages',
-  'glob',
   'html-webpack-plugin',
-  'intl',
   'jest',
-  'jsdom',
-  'ky',
   'lerna',
   'link-parent-bin',
-  'lodash.groupby',
-  'lodash.map',
-  'lodash.mapkeys',
-  'lodash.mapvalues',
   'memory-fs',
-  'mocha',
-  'nock',
-  'np',
-  'one-cerner-style-icons',
   'postcss',
   'react',
   'react-dom',
   'react-intl',
-  'react-test-renderer',
-  'regenerator-runtime',
-  'shelljs',
-  'sinon',
-  'strip-ansi',
   'stylelint',
-  'svgo',
-  'terra-application',
-  'terra-button',
-  'terra-disclosure-manager',
-  'terra-enzyme-intl',
   'utf-8-validate',
   'webpack',
   'webpack-cli',
   'webpack-dev-server',
-  'webpack-merge',
+  '@cerner/terra-cli',
+  'strip-ansi',
   'yargs',
+  'chai',
+  'lodash.groupby',
+  'lodash.map',
+  'lodash.mapkeys',
+  'lodash.mapvalues',
+  'mocha',
+  'nock',
+  'np',
+  'sinon',
+  '@babel/plugin-transform-object-assign',
+  '@cerner/browserslist-config-terra',
+  '@cerner/eslint-config-terra',
+  '@cerner/jest-config-terra',
+  '@cerner/stylelint-config-terra',
+  '@cerner/terra-aggregate-translations',
+  '@cerner/terra-functional-testing',
+  '@cerner/terra-open-source-scripts',
+  '@cerner/webpack-config-terra',
+  'check-installed-dependencies',
+  'core-js',
+  'gh-pages',
+  'glob',
+  'ky',
+  'react-test-renderer',
+  'regenerator-runtime',
+  'terra-application',
+  'terra-disclosure-manager',
+  'terra-enzyme-intl',
+  'webpack-merge',
+  'csvtojson',
+  'jsdom',
+  'one-cerner-style-icons',
+  'shelljs',
+  'svgo',
+  'terra-button',
 ];
 
 module.exports = {
@@ -82,7 +80,7 @@ module.exports = {
       if (projectType !== 'devModule') {
         const currentProblems = Object.keys(dependencies).map((dependency) => {
           const dependencyVersion = dependencies[dependency];
-          if (devPeerDependencySet.includes(dependency)) {
+          if (devDependencySet.includes(dependency)) {
             return `${dependency}@${dependencyVersion} does not satisfy requirement for ${messageString} rule.`;
           }
           return undefined;
@@ -90,6 +88,25 @@ module.exports = {
 
         if (currentProblems.length) {
           const lintMessage = `This project has unnecessary dependencies that violates the ${messageString} rule:\n  ${currentProblems.join('\n  ')}`;
+          report({
+            lintId: messageString, severity: ruleConfig.severity.severityType, lintMessage, projectType,
+          });
+        }
+      }
+    },
+    peerDependencies: (peerDependencies) => {
+      const messageString = 'require-no-unnecessary-dependencies';
+      if (projectType !== 'devModule') {
+        const currentProblems = Object.keys(peerDependencies).map((dependency) => {
+          const dependencyVersion = peerDependencies[dependency];
+          if (devDependencySet.includes(dependency)) {
+            return `${dependency}@${dependencyVersion} does not satisfy requirement for ${messageString} rule.`;
+          }
+          return undefined;
+        }).filter(problem => !!problem);
+
+        if (currentProblems.length) {
+          const lintMessage = `This project has unnecessary peerDependencies that violates the ${messageString} rule:\n  ${currentProblems.join('\n  ')}`;
           report({
             lintId: messageString, severity: ruleConfig.severity.severityType, lintMessage, projectType,
           });
