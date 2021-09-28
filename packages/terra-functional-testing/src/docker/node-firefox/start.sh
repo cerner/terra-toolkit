ad0d838230905697429d1f50b84b4ad83c496b54
Ryan Manuel <rfmanuel@gmail.com>
2021-09-28 13:29:16 -0500

    Publish on 9-28-2021

--- packages/terra-functional-testing/src/docker/node-firefox/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Inherit the REMOTE_HOST attribute from Marathon ENV Variables
-if [ ! -z "$HOST" ] && [ ! -z "$PORT" ]; then
-  export REMOTE_HOST="http://$HOST:$PORT"
-fi
-
-# Call the original Entrypoint
-source /opt/bin/entry_point.sh
