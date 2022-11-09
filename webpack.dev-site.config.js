61bdae7e373980758f6d69e8fe3166f394641e75
Karlo Seler <karlo.seler@primark.onmicrosoft.com>
2022-11-09 11:28:11 +0100

    UXPLATFORM-6979: Node 16 Upgrade

--- webpack.dev-site.config.js
+++ webpack.dev-site.config.js
@@ -4,18 +4,35 @@ const {
   TerraDevSite,
 } = require('@cerner/terra-dev-site');
 
-const WebpackConfigTerra = require('./packages/webpack-config-terra/lib/webpack.config');
+const WebpackConfigTerra = require('@cerner/webpack-config-terra');
 
 const coreConfig = (env = {}) => ({
   plugins: [
     new TerraDevSite({
       defaultLocale: env.defaultLocale,
+      primaryNavigationItems: [{
+        path: '/home',
+        text: 'Home',
+        contentExtension: 'home',
+      }, {
+        path: '/components',
+        text: 'Components',
+        contentExtension: 'doc',
+      }, {
+        path: '/dev_tools',
+        text: 'Developer Tools',
+        contentExtension: 'tool',
+      }, {
+        path: '/tests',
+        text: 'Tests',
+        contentExtension: 'test',
+      }],
     }),
   ],
 });
 
 const mergedConfig = (env, argv) => (
-  merge(WebpackConfigTerra(env, argv), coreConfig())
+  merge(WebpackConfigTerra(env, argv), coreConfig(env))
 );
 
 module.exports = mergedConfig;
