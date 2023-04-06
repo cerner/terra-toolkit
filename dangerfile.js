4cda386446d894c58fe57b25f116caa7f96142f0
Sydney Combs <42594047+sycombs@users.noreply.github.com>
2023-04-06 13:37:49 -0500

    Update dangerfile (#780)

--- dangerfile.js
+++ dangerfile.js
@@ -12,6 +12,7 @@ if (lernaConfig.command) {
 const CHANGELOG_PATTERN = /^packages\/([a-z-])*\/CHANGELOG\.md/i;
 
 const changedFiles = danger.git.created_files.concat(danger.git.modified_files);
+const allowedFilepaths = ['package.json', 'CHANGELOG.md', 'src', 'translations', 'terra-toolkit-docs'];
 
 const changedChangelogs = new Set();
 const changedPackages = new Set();
@@ -33,16 +34,24 @@ changedFiles.forEach((file) => {
     return;
   }
 
-  if (CHANGELOG_PATTERN.test(file)) {
-    changedChangelogs.add(packageName);
-  } else { // file is in a package and was changed - we need a changelog
-    changedPackages.add(packageName);
+  if (!allowedFilepaths.some(filepath => file.includes(filepath))) {
+    // skip further processing if the changed file is not among the allowed filepaths
+    return;
   }
+
+  if (CHANGELOG_PATTERN.test(file)) {
+    // changed file is the CHANGELOG itself
+    changedChangelogs.add(packageName);
+    return;
+  }
+
+  // for other allowed files
+  changedPackages.add(packageName);
 });
 
 const missingChangelogs = [...changedPackages].filter(packageName => !changedChangelogs.has(packageName));
 
 // Fail if there are package changes without a CHANGELOG update
 if (missingChangelogs.length > 0) {
-  fail(`Please include a CHANGELOG entry for each changed package on this PR. Looks like a CHANGELOG is missing for: \n\n - ${missingChangelogs.join('\n - ')}`);
+  fail(`Please include a CHANGELOG entry for each changed package on this PR. Looks like a CHANGELOG entry is missing for: \n\n - ${missingChangelogs.join('\n - ')}`);
 }
