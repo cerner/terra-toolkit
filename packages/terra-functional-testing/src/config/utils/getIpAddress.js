6af6f15a5bdac9a85dfb0fe434180079e2c9756d
SM051274 <sm051274@cerner.net>
2023-04-14 01:02:19 +0530

    removes hardcoded utun3 value to keep it llok for all the UTN interfaces to connect

--- packages/terra-functional-testing/src/config/utils/getIpAddress.js
+++ packages/terra-functional-testing/src/config/utils/getIpAddress.js
@@ -2,15 +2,16 @@ const ip = require('ip');
 const os = require('os');
 
 module.exports = () => {
-  const utun = Object.entries(os.networkInterfaces()).find(([key]) => key.includes(('utun3')));
+  const utun = Object.entries(os.networkInterfaces()).filter(([key]) => key.includes(('utun'))).flat();
+
   if (utun && utun.length > 0) {
-    let ipv4;
+    let IPv4;
     utun.forEach((utunItem) => {
       if (Array.isArray(utunItem)) {
-        ipv4 = utunItem.filter((networkItem) => networkItem.family === 'IPv4');
+        IPv4 = utunItem.filter((networkItem) => networkItem.family === 'IPv4');
       }
     });
-    return ipv4[0].address;
+    return (IPv4) ? IPv4[0].address : ip.address();
   }
 
   return ip.address();
