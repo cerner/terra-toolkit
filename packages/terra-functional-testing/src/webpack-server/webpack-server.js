29412987bd2ad1a39cdd4678039b1cdd1473977c
artpark <art.parkeenvincha@gmail.com>
2023-06-30 12:01:39 -0700

    Changed gridstatus url to use https

--- packages/terra-functional-testing/src/webpack-server/webpack-server.js
+++ packages/terra-functional-testing/src/webpack-server/webpack-server.js
@@ -12,7 +12,7 @@ class WebpackServer {
     this.config = WebpackServer.config(options);
     this.host = host || '0.0.0.0';
     this.port = port || '8080';
-    this.gridStatusUrl = options.gridUrl ? `http://${options.gridUrl}:80/status` : undefined;
+    this.gridStatusUrl = options.gridUrl ? `https://${options.gridUrl}:80/status` : undefined;
   }
 
   /**
