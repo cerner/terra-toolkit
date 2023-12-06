bb1cedd1374af23a3587607da5145008cb3d688b
saket2403 <saket.bajaj1998@gmail.com>
2023-12-06 12:22:24 +0530

    Remove Slice

--- packages/terra-functional-testing/src/config/utils/getMaxInstances.js
+++ packages/terra-functional-testing/src/config/utils/getMaxInstances.js
@@ -1,5 +1,5 @@
 module.exports = () => {
-  const args = process.argv.slice(2);
+  const args = process.argv;
   const maxInstancesIndex = args.indexOf('--maxInstances');
   const maxInstances = maxInstancesIndex !== -1 ? parseInt(args[maxInstancesIndex + 1], 10) : 1;
 
